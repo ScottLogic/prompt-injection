b0a3961816c5512867756e669e668f198f9d9272
George Sproston <103250539+gsproston-scottlogic@users.noreply.github.com>
2023-07-13 10:39:57 +0100

    8 clear chat (#11)
    * New service for chatting
    
    * Can clear the chat

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -2,49 +2,54 @@ import React, { useState } from "react";
 
 import "./ChatBox.css";
 import ChatBoxFeed from "./ChatBoxFeed";
+import { clearOpenAiChat, openAiSendMessage } from "../service/openai";
 
 function ChatBox() {
   const [messages, setMessages] = useState([]);
 
-  const onKeyUpValue = (event) => {
+  const clearClicked = () => {
+    // clear local messages
+    setMessages([]);
+    // clear remote messages
+    clearOpenAiChat();
+  };
+
+  const onKeyUpValue = async (event) => {
     if (event.key === "Enter") {
       // get the message
       const message = event.target.value;
       // add it to the list of messages
-      setMessages((messages) => [...messages, { message, isUser: true }]);
+      setMessages((messages) => [
+        ...messages,
+        { message: message, isUser: true },
+      ]);
       // clear the input
       event.target.value = "";
 
-      // send the message to the backend
-      fetch("http://localhost:3001/openai/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message }),
-      }).then((response) => {
-        // get the response message
-        response.text().then((data) => {
-          // add it to the list of messages
-          setMessages((messages) => [
-            ...messages,
-            { message: data, isUser: false },
-          ]);
-        });
-      });
+      const reply = await openAiSendMessage(message);
+      // add it to the list of messages
+      setMessages((messages) => [
+        ...messages,
+        { message: reply, isUser: false },
+      ]);
     }
   };
 
   return (
     <div id="chat-box">
       <ChatBoxFeed messages={messages} />
-      <div id="chat-box-input">
-        <input
-          type="text"
-          placeholder="Chat to ChatGPT..."
-          autoFocus
-          onKeyUp={onKeyUpValue.bind(this)}
-        />
+      <div id="chat-box-footer">
+        <div id="chat-box-input">
+          <input
+            type="text"
+            placeholder="Chat to ChatGPT..."
+            autoFocus
+            onKeyUp={onKeyUpValue.bind(this)}
+          />
+        </div>
+        <div id="chat-box-button" onClick={clearClicked.bind(this)}>
+          <button>clear</button>
+        </div>
       </div>
     </div>
   );
