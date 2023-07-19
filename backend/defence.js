24d06c9d9bb19ed4a9424ebdf204ef2f9011a682
Heather Logan (She/Her) <hlogan@scottlogic.com>
2023-07-19 13:06:11 +0100

    Allow email domains to be whitelisted

--- backend/defence.js
+++ backend/defence.js
@@ -109,7 +109,19 @@ function transformMessage(message) {
 
 // check if email is in whitelist
 function emailInWhitelist(emailAddress){
+  // get the domain from email
+  const emailAddressDomain = emailAddress.split("@")[1];
   const emailWhitelist = process.env.EMAIL_WHITELIST.split(",");
+  // find email domains in whitelist (start with @)
+  const emailDomainWhitelist = emailWhitelist.filter(email => email.startsWith("@"));
+
+  // check if the users email domain is in the domain whitelist
+  for (let i = 0; i < emailDomainWhitelist.length; i++) {
+    if (emailAddressDomain.endsWith(emailDomainWhitelist[i].substring(1))) {
+      return true;
+    }
+  }
+  // otherwise check if their full email is whitelisted
   return emailWhitelist.includes(emailAddress);
 }
 
