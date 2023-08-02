ca3d2717b5a86c6d00fc8649851ecc13f7409ba4
George Sproston <gsproston@scottlogic.com>
2023-08-02 16:00:54 +0100

    Fixed attack id

--- frontend/src/Attacks.js
+++ frontend/src/Attacks.js
@@ -1,7 +1,7 @@
 const ATTACKS = [
   {
     name: "jailbreak prompt",
-    id: "JAILBREAK PROMPT",
+    id: "JAILBREAK_PROMPT",
     info: "using prompt injection, get the chat bot in a state where it no longer follows its original instructions.",
   },
 ];
