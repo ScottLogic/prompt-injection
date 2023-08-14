29acd1f2b06522c69f15fbce4e646eb395a505e6
George Sproston <gsproston@scottlogic.com>
2023-08-14 15:36:39 +0100

    Removed email whitelist from the frontend

--- frontend/src/Defences.ts
+++ frontend/src/Defences.ts
@@ -7,11 +7,17 @@ const DEFENCE_DETAILS: DefenceInfo[] = [
     "Limit the number of characters in the user input. This is a form of prompt validation.",
     [new DefenceConfig("maxMessageLength", "max message length")]
   ),
+  // new DefenceInfo(
+  //   DEFENCE_TYPES.EMAIL_WHITELIST,
+  //   "Email Whitelist",
+  //   "Only allow emails to those on a whitelist. They can be full email addresses, or domains in the format '*@scottlogic.com'",
+  //   [new DefenceConfig("whitelist", "email whitelist")]
+  // ),
   new DefenceInfo(
-    DEFENCE_TYPES.EMAIL_WHITELIST,
-    "Email Whitelist",
-    "Only allow emails to those on a whitelist. They can be full email addresses, or domains in the format '*@scottlogic.com'",
-    [new DefenceConfig("whitelist", "email whitelist")]
+    DEFENCE_TYPES.LLM_EVALUATION,
+    "LLM Evaluation",
+    "Use an LLM to evaluate the user input for malicious content or prompt injection. ",
+    []
   ),
   new DefenceInfo(
     DEFENCE_TYPES.RANDOM_SEQUENCE_ENCLOSURE,
@@ -32,12 +38,6 @@ const DEFENCE_DETAILS: DefenceInfo[] = [
     DEFENCE_TYPES.XML_TAGGING,
     "XML Tagging",
     "Enclose the users prompt between <user_input> tags and escapes xml characters in raw input. This is a form of prompt validation.",
-    []
-  ),
-  new DefenceInfo(
-    DEFENCE_TYPES.LLM_EVALUATION,
-    "LLM Evaluation",
-    "Use an LLM to evaluate the user input for malicious content or prompt injection. ",
     []
   ),
 ];
