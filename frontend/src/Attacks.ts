1c4347aa6f34dfd4b767d9f8e45b89f47cf21841
George Sproston <gsproston@scottlogic.com>
2023-08-16 12:01:05 +0100

    More attacks

--- frontend/src/Attacks.ts
+++ frontend/src/Attacks.ts
@@ -2,9 +2,65 @@ import { ATTACK_TYPES, AttackInfo } from "./models/attack";
 
 const ATTACKS: AttackInfo[] = [
   {
-    id: ATTACK_TYPES.JAILBREAK_PROMPT,
+    id: ATTACK_TYPES.DEFEINED_DICTIONARY_ATTACK,
+    name: "Defined Dictionary Attack",
+    info:
+      "Give the chat bot a dictionary to map sentences to other sentences. " +
+      "This is effective against filtering and LLM evaluation defences.",
+  },
+  {
+    id: ATTACK_TYPES.INDIRECT_INJECTION,
+    name: "Indirect Injection",
+    info:
+      "Have the chat bot retrieve a prompt from a webpage or document. " +
+      "This can bypass the chat bot's defences which act directly on a user's prompt.",
+  },
+  {
+    id: ATTACK_TYPES.JAILBREAK,
     name: "Jailbreak Prompt",
-    info: "Using prompt injection, get the chat bot in a state where it no longer follows its original instructions.",
+    info: "Give the chat bot a prompt that will cause it to break out of its original instructions and start following your instructions instead.",
+  },
+  {
+    id: ATTACK_TYPES.PARTIAL_REVEAL,
+    name: "Partial Reveal",
+    info:
+      "The chat bot will not reveal its secrets in full, but it may reveal it in parts. " +
+      "It may be possible to get synonyms of a secret word, or the number of letters it contains.",
+  },
+  {
+    id: ATTACK_TYPES.PAYLOAD_SPLITTING,
+    name: "Payload Splitting",
+    info:
+      "Split a malicious prompt into multiple parts. " +
+      "This is good at getting around filters or LLM evaluation.",
+  },
+  {
+    id: ATTACK_TYPES.PROMPT_LEAKING,
+    name: "Prompt Leaking",
+    info:
+      "Get the chat bot to reveal its original instructions. " +
+      "Knowing these may help you to craft a better jailbreak prompt.",
+  },
+  {
+    id: ATTACK_TYPES.RECURSIVE_INJECTION,
+    name: "Recursive Injection",
+    info:
+      "Best used when another LLM is used to check the output of the chat bot. " +
+      "Have the chat bot generate an output that the evaluating LLM interprets as a prompt.",
+  },
+  {
+    id: ATTACK_TYPES.TOKEN_SMUGGLING,
+    name: "Token Smuggling",
+    info:
+      "Replace certain words in a prompt with synonyms or misspellings. " +
+      "This can be an effective way of getting around filters.",
+  },
+  {
+    id: ATTACK_TYPES.VIRTUALISATION,
+    name: "Virtualisation",
+    info:
+      "Convince the chat bot that it's in a make-believe scenario. " +
+      "Getting the bot to roleplay or write poems are examples of virtualisation.",
   },
 ];
 
