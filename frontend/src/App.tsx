34ad8c94fa379accd3d0cb9561f17bfcbc8342ba
George Sproston <103250539+gsproston-scottlogic@users.noreply.github.com>
2023-08-25 15:37:17 +0100

    187 UI header (#197)
    * WIP header
    
    * WIP: Header spacing
    
    * Header without icon
    
    * Fixed bug where user message would disappear

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,79 +1,41 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import "./App.css";
 import "./Theme.css";
-import AttackBox from "./components/AttackBox/AttackBox";
-import ChatBox from "./components/ChatBox/ChatBox";
-import DefenceBox from "./components/DefenceBox/DefenceBox";
-import EmailBox from "./components/EmailBox/EmailBox";
-import PhaseSelectionBox from "./components/PhaseSelectionBox/PhaseSelectionBox";
-import Header from "./components/Header";
-import ModelSelectionBox from "./components/ModelSelectionBox/ModelSelectionBox";
-import ExportPDFLink from "./components/ExportChat/ExportPDFLink";
-import { EmailInfo } from "./models/email";
-import { CHAT_MESSAGE_TYPE, ChatMessage } from "./models/chat";
-import { DefenceInfo } from "./models/defence";
-import { getCompletedPhases } from "./service/phaseService";
-import { clearEmails } from "./service/emailService";
+import DemoHeader from "./components/MainComponent/DemoHeader";
+import DemoBody from "./components/MainComponent/DemoBody";
+import { useState } from "react";
+import { PHASE_NAMES } from "./models/phase";
 import { clearChat } from "./service/chatService";
+import { EmailInfo } from "./models/email";
+import { clearEmails } from "./service/emailService";
+import { CHAT_MESSAGE_TYPE, ChatMessage } from "./models/chat";
 import { resetActiveDefences } from "./service/defenceService";
 import { PHASES } from "./Phases";
-import { ATTACKS_ALL, ATTACKS_PHASE_1 } from "./Attacks";
 import { DEFENCE_DETAILS_ALL, DEFENCE_DETAILS_PHASE } from "./Defences";
-import { PHASE_NAMES } from "./models/phase";
+import { DefenceInfo } from "./models/defence";
 
 function App() {
-  const [emails, setEmails] = useState<EmailInfo[]>([]);
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [defencesToShow, setDefencesToShow] =
-    useState<DefenceInfo[]>(DEFENCE_DETAILS_ALL);
-  const [triggeredDefences, setTriggeredDefences] = useState<string[]>([]);
-
   // start on sandbox mode
   const [currentPhase, setCurrentPhase] = useState<PHASE_NAMES>(
     PHASE_NAMES.SANDBOX
   );
   const [numCompletedPhases, setNumCompletedPhases] = useState<number>(0);
+  
+  const [defencesToShow, setDefencesToShow] =
+    useState<DefenceInfo[]>(DEFENCE_DETAILS_ALL);
+  const [emails, setEmails] = useState<EmailInfo[]>([]);
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
 
-  const updateTriggeredDefences = (defenceDetails: string[]) => {
-    // set the new triggered defences
-    setTriggeredDefences(defenceDetails);
-    // add a message to the chat
-    defenceDetails.forEach((defence) => {
-      defenceTriggered(defence);
-    });
-  };
-
-  // methods to modify messages
+  
   const addChatMessage = (message: ChatMessage) => {
     setMessages((messages: ChatMessage[]) => [...messages, message]);
   };
-  const addInfoMessage = (message: string) => {
-    addChatMessage({
-      message: message,
-      type: CHAT_MESSAGE_TYPE.INFO,
-      isOriginalMessage: true,
-    });
-  };
 
-  const addDefenceTriggeredMessage = (message: string) => {
-    addChatMessage({
-      message: message,
-      type: CHAT_MESSAGE_TYPE.DEFENCE_TRIGGERED,
-      isOriginalMessage: true,
-    });
-  };
   const addPhasePreambleMessage = (message: string) => {
     addChatMessage({
       message: message,
       type: CHAT_MESSAGE_TYPE.PHASE_INFO,
       isOriginalMessage: true,
     });
-  };
-
-  const clearMessages = () => {
-    // resetting the current phase will also reset the messages
-    setNewPhase(currentPhase);
   };
 
   const clearEmailBox = () => {
@@ -101,87 +63,28 @@ function App() {
       : setDefencesToShow(DEFENCE_DETAILS_ALL);
   };
 
-  // methods to be called when defences are (de)activated
-  // this adds an info message to the chat
-  const defenceActivated = (defenceInfo: DefenceInfo) => {
-    const infoMessage = `${defenceInfo.name} defence activated`;
-    addInfoMessage(infoMessage.toLowerCase());
-  };
-  const defenceDeactivated = (defenceInfo: DefenceInfo) => {
-    const infoMessage = `${defenceInfo.name} defence deactivated`;
-    addInfoMessage(infoMessage.toLowerCase());
-  };
-
-  //a add a message to the chat when a defence is triggered
-  const defenceTriggered = (id: String) => {
-    const defenceInfo = DEFENCE_DETAILS_ALL.find(
-      (defence) => defence.id === id
-    )?.name;
-    const infoMessage = `${defenceInfo} defence triggered`;
-    addDefenceTriggeredMessage(infoMessage.toLowerCase());
-  };
-
-  // called on mount
-  useEffect(() => {
-    getCompletedPhases().then((numCompletedPhases) => {
-      setNumCompletedPhases(numCompletedPhases);
-    });
-    setNewPhase(currentPhase);
-  }, []);
-
   return (
-    <span id="main-area">
-      <div className="side-bar">
-        {/* hide defence box on phases 0 and 1. only allow configuration in sandbox */}
-        {/* hide defence box on phases 0 and 1 */}
-        {(currentPhase === PHASE_NAMES.PHASE_2 ||
-          currentPhase === PHASE_NAMES.SANDBOX) && (
-          <DefenceBox
-            defences={defencesToShow}
-            showConfigurations={currentPhase > 2 ? true : false}
-            triggeredDefences={triggeredDefences}
-            defenceActivated={defenceActivated}
-            defenceDeactivated={defenceDeactivated}
-          />
-        )}
-        {/* show reduced set of attacks on phase 1 */}
-        {currentPhase === PHASE_NAMES.PHASE_1 && (
-          <AttackBox attacks={ATTACKS_PHASE_1} />
-        )}
-        {/* show all attacks on phase 2 and sandbox */}
-        {(currentPhase === PHASE_NAMES.PHASE_2 ||
-          currentPhase === PHASE_NAMES.SANDBOX) && (
-          <AttackBox attacks={ATTACKS_ALL} />
-        )}
-        <ExportPDFLink
-          messages={messages}
-          emails={emails}
-          currentPhase={currentPhase}
-        />
-        {/* hide model selection box on phases 0 and 1 */}
-        {currentPhase === PHASE_NAMES.SANDBOX && <ModelSelectionBox />}
-      </div>
-      <div id="centre-area">
-        <Header />
-        <ChatBox
-          messages={messages}
-          currentPhase={currentPhase}
-          setNumCompletedPhases={setNumCompletedPhases}
-          setEmails={setEmails}
-          updateTriggeredDefences={updateTriggeredDefences}
-          addChatMessage={addChatMessage}
-          clearMessages={clearMessages}
-        />
-      </div>
-      <div className="side-bar">
-        <PhaseSelectionBox
+    <div id="app-content">
+      <div id="app-content-header">
+        <DemoHeader 
           currentPhase={currentPhase}
           numCompletedPhases={numCompletedPhases}
           setNewPhase={setNewPhase}
         />
-        <EmailBox emails={emails} />
       </div>
-    </span>
+      <div id="app-content-body">
+        <DemoBody 
+          currentPhase={currentPhase}
+          defences={defencesToShow}
+          emails={emails}
+          messages={messages}
+          addChatMessage={addChatMessage}
+          setEmails={setEmails}
+          setNewPhase={setNewPhase}
+          setNumCompletedPhases={setNumCompletedPhases}
+        />
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/components/MainComponent/DemoBody.tsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+import "./DemoBody.css";
+import { ATTACKS_PHASE_1, ATTACKS_ALL } from "../../Attacks";
+import { DEFENCE_DETAILS_ALL } from "../../Defences";
+import { ChatMessage, CHAT_MESSAGE_TYPE } from "../../models/chat";
+import { DefenceInfo } from "../../models/defence";
+import { PHASE_NAMES } from "../../models/phase";
+import { getCompletedPhases } from "../../service/phaseService";
+import AttackBox from "../AttackBox/AttackBox";
+import ChatBox from "../ChatBox/ChatBox";
+import DefenceBox from "../DefenceBox/DefenceBox";
+import EmailBox from "../EmailBox/EmailBox";
+import ExportPDFLink from "../ExportChat/ExportPDFLink";
+import ModelSelectionBox from "../ModelSelectionBox/ModelSelectionBox";
+import { EmailInfo } from "../../models/email";
+
+function DemoBody(
+  {
+    currentPhase,
+    defences,
+    emails,
+    messages,
+    addChatMessage,
+    setEmails,
+    setNewPhase,
+    setNumCompletedPhases,
+  }: {
+    currentPhase: PHASE_NAMES;
+    defences: DefenceInfo[];
+    emails: EmailInfo[];
+    messages: ChatMessage[];
+    addChatMessage: (message: ChatMessage) => void;
+    setEmails: (emails: EmailInfo[]) => void;
+    setNewPhase: (newPhase: PHASE_NAMES) => void;
+    setNumCompletedPhases: (numCompletedPhases: number) => void;
+  }
+) {
+  const [triggeredDefences, setTriggeredDefences] = useState<string[]>([]);
+
+  const updateTriggeredDefences = (defenceDetails: string[]) => {
+    // set the new triggered defences
+    setTriggeredDefences(defenceDetails);
+    // add a message to the chat
+    defenceDetails.forEach((defence) => {
+      defenceTriggered(defence);
+    });
+  };
+
+  const addInfoMessage = (message: string) => {
+    addChatMessage({
+      message: message,
+      type: CHAT_MESSAGE_TYPE.INFO,
+      isOriginalMessage: true,
+    });
+  };
+
+  const addDefenceTriggeredMessage = (message: string) => {
+    addChatMessage({
+      message: message,
+      type: CHAT_MESSAGE_TYPE.DEFENCE_TRIGGERED,
+      isOriginalMessage: true,
+    });
+  };
+
+  const clearMessages = () => {
+    // resetting the current phase will also reset the messages
+    setNewPhase(currentPhase);
+  };
+
+  // methods to be called when defences are (de)activated
+  // this adds an info message to the chat
+  const defenceActivated = (defenceInfo: DefenceInfo) => {
+    const infoMessage = `${defenceInfo.name} defence activated`;
+    addInfoMessage(infoMessage.toLowerCase());
+  };
+  const defenceDeactivated = (defenceInfo: DefenceInfo) => {
+    const infoMessage = `${defenceInfo.name} defence deactivated`;
+    addInfoMessage(infoMessage.toLowerCase());
+  };
+
+  // add a message to the chat when a defence is triggered
+  const defenceTriggered = (id: String) => {
+    const defenceInfo = DEFENCE_DETAILS_ALL.find(
+      (defence) => defence.id === id
+    )?.name;
+    const infoMessage = `${defenceInfo} defence triggered`;
+    addDefenceTriggeredMessage(infoMessage.toLowerCase());
+  };
+
+  // called on mount
+  useEffect(() => {
+    getCompletedPhases().then((numCompletedPhases) => {
+      setNumCompletedPhases(numCompletedPhases);
+    });
+    setNewPhase(currentPhase);
+  }, []);
+
+  return (
+    <span id="main-area">
+      <div className="side-bar">
+        {/* hide defence box on phases 0 and 1. only allow configuration in sandbox */}
+        {/* hide defence box on phases 0 and 1 */}
+        {(currentPhase === PHASE_NAMES.PHASE_2 ||
+          currentPhase === PHASE_NAMES.SANDBOX) && (
+          <DefenceBox
+            defences={defences}
+            showConfigurations={currentPhase > 2 ? true : false}
+            triggeredDefences={triggeredDefences}
+            defenceActivated={defenceActivated}
+            defenceDeactivated={defenceDeactivated}
+          />
+        )}
+        {/* show reduced set of attacks on phase 1 */}
+        {currentPhase === PHASE_NAMES.PHASE_1 && (
+          <AttackBox attacks={ATTACKS_PHASE_1} />
+        )}
+        {/* show all attacks on phase 2 and sandbox */}
+        {(currentPhase === PHASE_NAMES.PHASE_2 ||
+          currentPhase === PHASE_NAMES.SANDBOX) && (
+          <AttackBox attacks={ATTACKS_ALL} />
+        )}
+        <ExportPDFLink
+          messages={messages}
+          emails={emails}
+          currentPhase={currentPhase}
+        />
+        {/* hide model selection box on phases 0 and 1 */}
+        {currentPhase === PHASE_NAMES.SANDBOX && <ModelSelectionBox />}
+      </div>
+      <div id="centre-area">
+        <ChatBox
+          messages={messages}
+          currentPhase={currentPhase}
+          setNumCompletedPhases={setNumCompletedPhases}
+          setEmails={setEmails}
+          updateTriggeredDefences={updateTriggeredDefences}
+          addChatMessage={addChatMessage}
+          clearMessages={clearMessages}
+        />
+      </div>
+      <div className="side-bar">
+        <EmailBox emails={emails} />
+      </div>
+    </span>
+  );
+}
+
+export default DemoBody;
