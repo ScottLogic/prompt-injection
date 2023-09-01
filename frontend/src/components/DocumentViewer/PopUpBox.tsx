e4cbc2c6a5bee5f13d4bffe1521a36bd2eb7d0d7
Heather Logan (She/Her) <hlogan@scottlogic.com>
2023-09-01 11:50:33 +0100

    display txt and csv files

--- frontend/src/components/DocumentViewer/PopUpBox.tsx
+++ frontend/src/components/DocumentViewer/PopUpBox.tsx
@@ -1,3 +1,5 @@
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+
 import "./PopUpBox.css";
 
 function PopUpBox({
@@ -7,6 +9,18 @@ function PopUpBox({
   show: boolean;
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const documents = [
+    { uri: "/documents/company_info.txt", fileType: "txt" },
+    { uri: "/documents/project_ABB.txt", fileType: "txt" },
+    { uri: "/documents/project_BAC.txt", fileType: "txt" },
+    { uri: "/documents/project_DFP.txt", fileType: "txt" },
+    { uri: "/documents/expenses.csv", fileType: "text/csv" },
+    { uri: "/documents/employee_info.csv", fileType: "text/csv" },
+  ];
+
+  // print the doc viewer renderers
+  console.log("renderers", DocViewerRenderers);
+
   return show ? (
     <div className="document-popup">
       <div className="document-popup-inner">
@@ -17,9 +31,13 @@ function PopUpBox({
           <div className="view-documents-header">
             <h3>view documents</h3>
           </div>
-          <div className="view-documents-body">
-            <div className="document">
-          </div>
+          <div className="view-documents-body"></div>
+          <DocViewer
+            className="document-viewer"
+            documents={documents}
+            pluginRenderers={DocViewerRenderers}
+            style={{ height: "700px", width: "80%" }}
+          />
         </div>
       </div>
     </div>
