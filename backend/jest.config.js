82e8e22f9e3ab9a85b1e1211d950d71cc0b9054e
George Sproston <gsproston@scottlogic.com>
2023-09-04 16:50:19 +0100

    Excluding build files from testing

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  modulePathIgnorePatterns: ["build", "coverage", "node_modules"],
   preset: "ts-jest",
   testEnvironment: "node",
 };
