0533fc29f343aa87531dbc34e3526cb6aa5be2b4
George Sproston <gsproston@scottlogic.com>
2023-09-11 13:23:49 +0100

    React-friendly icon

--- frontend/src/components/MainComponent/ProjectIcon.tsx
+++ frontend/src/components/MainComponent/ProjectIcon.tsx
@@ -40,8 +40,8 @@ function ProjectIcon() {
         fill="white"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M191.775 322.266C285.451 322.266 361.391 265.522 361.391 195.525C361.391 125.528 285.451 68.7837 191.775 68.7837C98.0993 68.7837 22.1599 125.528 22.1599 195.525C22.1599 265.522 98.0993 322.266 191.775 322.266ZM192.151 304.966C275.649 304.966 343.338 261.861 343.338 208.688C343.338 155.514 275.649 112.409 192.151 112.409C108.653 112.409 40.964 155.514 40.964 208.688C40.964 261.861 108.653 304.966 192.151 304.966Z"
         fill="white"
       />
@@ -53,9 +53,9 @@ function ProjectIcon() {
         transform="matrix(-1 0 0 1 385 180.858)"
         fill="white"
       />
-      <circle cx="129" cy="201" r="34.5" stroke="#1FD07B" stroke-width="17" />
-      <circle cx="255" cy="201" r="34.5" stroke="#1FD07B" stroke-width="17" />
-      <line x1="194" y1="46" x2="194" y2="77" stroke="white" stroke-width="4" />
+      <circle cx="129" cy="201" r="34.5" stroke="#1FD07B" strokeWidth="17" />
+      <circle cx="255" cy="201" r="34.5" stroke="#1FD07B" strokeWidth="17" />
+      <line x1="194" y1="46" x2="194" y2="77" stroke="white" strokeWidth="4" />
     </svg>
   );
 }
