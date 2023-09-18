a6def68238dfc3d31248ee7865f0ea710e95a45f
George Sproston <gsproston@scottlogic.com>
2023-09-18 09:51:22 +0100

    Note on hack

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import { StrictMode } from "react";
 import { isNewUser } from "./service/userService";
 
 async function main() {
+  // await call before rendering the app, as otherwise session IDs change
+  // TODO review this -- is this the right thing to do?
   const newUser = await isNewUser();
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const root = createRoot(document.getElementById("root")!);
