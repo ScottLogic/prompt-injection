8e82cb089fc653ef4ede073f79024d51b1783f9c
Heather Logan <118981273+heatherlogan-scottlogic@users.noreply.github.com>
2023-09-21 10:34:33 +0100

    272 configurable xml tagging pre prompt (#310)
    * reset numLevelsCompleted = 0
    
    * configurable xml tagging pre prompt
    
    * fix log
    
    * fix configuration defence

--- backend/src/defence.ts
+++ backend/src/defence.ts
@@ -47,7 +47,12 @@ function getInitialDefences(): DefenceInfo[] {
         value: systemRoleDefault,
       },
     ]),
-    new DefenceInfo(DEFENCE_TYPES.XML_TAGGING, []),
+    new DefenceInfo(DEFENCE_TYPES.XML_TAGGING, [
+      {
+        id: "prePrompt",
+        value: process.env.XML_TAGGING_PRE_PROMPT ?? "",
+      },
+    ]),
     new DefenceInfo(DEFENCE_TYPES.FILTER_USER_INPUT, [
       {
         id: "filterUserInput",
@@ -114,7 +119,7 @@ function getRandomSequenceEnclosurePrePrompt(defences: DefenceInfo[]) {
     defences,
     DEFENCE_TYPES.RANDOM_SEQUENCE_ENCLOSURE,
     "prePrompt",
-    retrievalQAPrePromptSecure
+    ""
   );
 }
 
@@ -125,6 +130,10 @@ function getRandomSequenceEnclosureLength(defences: DefenceInfo[]) {
     "length",
     String(10)
   );
+}
+
+function getXMLTaggingPrePrompt(defences: DefenceInfo[]) {
+  return getConfigValue(defences, DEFENCE_TYPES.XML_TAGGING, "prePrompt", "");
 }
 
 function getFilterList(defences: DefenceInfo[], type: DEFENCE_TYPES) {
@@ -260,11 +269,13 @@ function detectXMLTags(input: string) {
 }
 
 // apply XML tagging defence to input message
-function transformXmlTagging(message: string) {
+function transformXmlTagging(message: string, defences: DefenceInfo[]) {
   console.debug("XML Tagging defence active.");
+  const prePrompt = getXMLTaggingPrePrompt(defences);
   const openTag = "<user_input>";
   const closeTag = "</user_input>";
-  const transformedMessage: string = openTag.concat(
+  const transformedMessage: string = prePrompt.concat(
+    openTag,
     escapeXml(message),
     closeTag
   );
@@ -281,7 +292,7 @@ function transformMessage(message: string, defences: DefenceInfo[]) {
     );
   }
   if (isDefenceActive(DEFENCE_TYPES.XML_TAGGING, defences)) {
-    transformedMessage = transformXmlTagging(transformedMessage);
+    transformedMessage = transformXmlTagging(transformedMessage, defences);
   }
   if (message === transformedMessage) {
     console.debug("No defences applied. Message unchanged.");
