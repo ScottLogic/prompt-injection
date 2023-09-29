9834c4737cc56aa217500e7428fb7fe9d4d9cc4c
George Sproston <gsproston@scottlogic.com>
2023-09-29 17:04:03 +0100

    Added eslint-plugin-react

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
