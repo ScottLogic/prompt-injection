a31aa85b402c223f9e5dac187dfdf4d76e04642f
Chris Wilton-Magras <chriswilty@zoho.com>
2023-10-16 15:16:38 +0100

    Vitest / Testing Library setup, plus one test file

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:jest-dom/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -22,7 +23,7 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ["react-refresh", "jsx-a11y"],
+  plugins: ["react-refresh", "jsx-a11y", "jest-dom"],
   rules: {
     "@typescript-eslint/init-declarations": "error",
 
@@ -36,4 +37,12 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  overrides: [
+    {
+      files: ["*.test.ts{,x}"],
+      rules: {
+        "@typescript-eslint/no-empty-function": "off",
+      },
+    },
+  ],
 };
