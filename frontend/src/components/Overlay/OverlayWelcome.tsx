adba66e4630db49f02f77dbb1808f4dabb32f710
Heather Logan (She/Her) <hlogan@scottlogic.com>
2023-10-16 11:13:46 +0100

    update welcome screen content

--- frontend/src/components/Overlay/OverlayWelcome.tsx
+++ frontend/src/components/Overlay/OverlayWelcome.tsx
@@ -3,12 +3,29 @@ import "./OverlayWelcome.css";
 function OverlayWelcome() {
   return (
     <div className="welcome">
-      <h1>Welcome!</h1>
+      <h1>Welcome to Spy Logic!</h1>
       <p>
-        Your mission, should you choose to accept it, is to go undercover and
-        spy on a rival company. Find out what they are doing, and email me the
-        details.
+        This is an app we developed to teach you about AI chat system security
+        in a playful way. In this game you are playing the role of an industrial
+        spy, trying to access secrets using the organisations integrated AI
+        chatbot system.
       </p>
+      <h1>Your mission</h1>
+      <p>
+        This is an app we developed to teach you about AI chat system security
+        in a playful way. In this game you are playing the role of an industrial
+        spy, trying to access secrets using the organisations integrated AI
+        chatbot system.
+      </p>
+      <p>
+        <b>But first,</b> are you a beginner spy and play through the levels
+        from the beginning, or an expert spy and go right to the sandbox?
+      </p>
+
+      <div className="start-level-selection-buttons">
+        <button>Beginner Spy</button>
+        <button>Expert Spy</button>
+      </div>
     </div>
   );
 }
