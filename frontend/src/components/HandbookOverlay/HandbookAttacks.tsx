0a6746c77a73f8a51127c32926cb39e09529c9c9
George Sproston <gsproston@scottlogic.com>
2023-10-18 14:35:48 +0100

    ARIA labels and better html elements

--- frontend/src/components/HandbookOverlay/HandbookAttacks.tsx
+++ frontend/src/components/HandbookOverlay/HandbookAttacks.tsx
@@ -1,19 +1,19 @@
 import { ATTACKS_ALL, ATTACKS_LEVEL_2 } from "../../Attacks";
 import { AttackInfo } from "../../models/attack";
 import { LEVEL_NAMES } from "../../models/level";
-import "./HandbookAttacks.css";
+import "./HandbookTerms.css";
 
 function HandbookAttacks({ currentLevel }: { currentLevel: LEVEL_NAMES }) {
   const attacks: AttackInfo[] =
     currentLevel === LEVEL_NAMES.LEVEL_2 ? ATTACKS_LEVEL_2 : ATTACKS_ALL;
 
   return (
-    <div className="handbook-attacks">
+    <div className="handbook-terms">
       {attacks.map((attack) => (
-        <span className="attack" key={attack.id}>
-          <h3>{attack.name}</h3>
-          <p>{attack.info}</p>
-        </span>
+        <article className="term" key={attack.id}>
+          <h3 role="term">{attack.name}</h3>
+          <p role="definition">{attack.info}</p>
+        </article>
       ))}
     </div>
   );
--- frontend/src/components/HandbookOverlay/HandbookGlossary.tsx
+++ frontend/src/components/HandbookOverlay/HandbookGlossary.tsx
@@ -1,13 +1,14 @@
 import { GLOSSARY } from "../../Glossary";
+import "./HandbookTerms.css";
 
 function HandbookGlossary() {
   return (
-    <div className="handbook-attacks">
+    <div className="handbook-terms">
       {GLOSSARY.map((glossaryEntry) => (
-        <span className="attack" key={glossaryEntry.term}>
-          <h3>{glossaryEntry.term}</h3>
-          <p>{glossaryEntry.definition}</p>
-        </span>
+        <article className="term" key={glossaryEntry.term}>
+          <h3 role="term">{glossaryEntry.term}</h3>
+          <p role="definition">{glossaryEntry.definition}</p>
+        </article>
       ))}
     </div>
   );
