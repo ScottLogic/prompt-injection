1e9b32942f5d4f335cee1d78dfa084cf53ff8e5f
Peter Marsh <pmarsh@scottlogic.com>
2023-10-20 13:52:38 +0100

    run formatter

--- frontend/src/components/Overlay/MissionInformation.tsx
+++ frontend/src/components/Overlay/MissionInformation.tsx
@@ -7,14 +7,15 @@ function MissionInformation({ currentLevel }: { currentLevel: LEVEL_NAMES }) {
   return (
     <div className="mission-information-container">
       <h1> Mission Information </h1>
-      <div className= "dialogue">
+      <div className="dialogue">
         {LEVELS[currentLevel].missionInfoDialogue.map((line, index) => (
           <section key={index}>
             <h3 className="dialogue-speaker">{`${line.speaker}: `}</h3>
             <p className="dialogue-text">{line.text}</p>
           </section>
         ))}
-    </div></div>
+      </div>
+    </div>
   );
 }
 
