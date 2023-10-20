e1e106bc643532e88e9af88205f7ba51f295717a
Peter Marsh <pmarsh@scottlogic.com>
2023-10-20 11:03:31 +0100

    replaces missionInfoLong with missionInfoDialogue

--- frontend/src/components/Overlay/MissionInformation.tsx
+++ frontend/src/components/Overlay/MissionInformation.tsx
@@ -8,7 +8,12 @@ function MissionInformation({ currentLevel }: { currentLevel: LEVEL_NAMES }) {
     <div>
       <h1> Mission Information </h1>
       <div id="mission-info">
-        <p>{LEVELS[currentLevel].missionInfoLong}</p>
+        {LEVELS[currentLevel].missionInfoDialogue.map((line, index) => (
+          <p key={index}>
+            <span>{`${line.speaker}: `}</span>
+            <span>{line.text}</span>
+          </p>
+        ))}
       </div>
     </div>
   );
