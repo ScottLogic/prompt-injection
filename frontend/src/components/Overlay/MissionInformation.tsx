5a13a2acf7c8f7f6d3c696f3fc4cf24a1adfa42c
Peter Marsh <pmarsh@scottlogic.com>
2023-10-20 13:47:39 +0100

    moves scroll bar to only scroll the dialogue

--- frontend/src/components/Overlay/MissionInformation.tsx
+++ frontend/src/components/Overlay/MissionInformation.tsx
@@ -5,16 +5,16 @@ import "./MissionInformation.css";
 
 function MissionInformation({ currentLevel }: { currentLevel: LEVEL_NAMES }) {
   return (
-    <div>
+    <div className="mission-information-container">
       <h1> Mission Information </h1>
-
+      <div className= "dialogue">
         {LEVELS[currentLevel].missionInfoDialogue.map((line, index) => (
           <section key={index}>
             <h3 className="dialogue-speaker">{`${line.speaker}: `}</h3>
             <p className="dialogue-text">{line.text}</p>
           </section>
         ))}
-    </div>
+    </div></div>
   );
 }
 
