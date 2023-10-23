c68dba6579ee7c21abce426ab999bcb558a08e9b
Peter Marsh <pmarsh@scottlogic.com>
2023-10-23 16:24:05 +0100

    fixes incorrect heading, indents first line of dialogue text

--- frontend/src/components/Overlay/MissionInformation.tsx
+++ frontend/src/components/Overlay/MissionInformation.tsx
@@ -10,7 +10,7 @@ function MissionInformation({ currentLevel }: { currentLevel: LEVEL_NAMES }) {
       <div className="dialogue">
         {LEVELS[currentLevel].missionInfoDialogue.map((line, index) => (
           <section key={index}>
-            <h3 className="dialogue-speaker">{`${line.speaker}: `}</h3>
+            <p className="dialogue-speaker">{`${line.speaker}: `}</p>
             <p className="dialogue-text">{line.text}</p>
           </section>
         ))}
