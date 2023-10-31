7fdcb321731ade8b0ae051cc5cc1d317f9f68103
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2023-10-31 17:15:38 +0000

    changed className to divider-line

--- frontend/src/components/Overlay/LevelsComplete.tsx
+++ frontend/src/components/Overlay/LevelsComplete.tsx
@@ -25,7 +25,7 @@ function LevelsComplete({
         <h1>Congratulations!</h1>
         <div className="content">
           <MissionDialogue dialogueLines={managerDialogue} />
-          <div className="sent-email-divider"></div>
+          <div className="divider-line"></div>
           <p>
             You&apos;ve completed the story mode! You can stay here and continue
             to play with the levels, or you can move onto Sandbox mode where you
