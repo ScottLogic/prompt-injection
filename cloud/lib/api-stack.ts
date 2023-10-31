669f7d12a3249147bb874e05f86a4be9bf8f6a58
Chris Wilton-Magras <chriswilty@zoho.com>
2023-10-31 15:59:08 +0000

    Use docker image, with apikey env Secret

--- cloud/lib/api-stack.ts
+++ cloud/lib/api-stack.ts
@@ -1,8 +1,9 @@
 import { Stack, StackProps } from "aws-cdk-lib/core";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
-//import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
-import { Cluster, ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
+import { Cluster, ContainerImage, Secret as EnvSecret } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
 import { resourceName, stageName } from "./resourceNamingUtils";
@@ -16,16 +17,18 @@ export class ApiStack extends Stack {
 
     const generateResourceName = resourceName(scope);
 
-    /*const dockerImageAsset = new DockerImageAsset(
+    const dockerImageAsset = new DockerImageAsset(
       this,
-      generateResourceName('docker-image'),
+      generateResourceName('container-image'),
       {
         directory: '../../backend',
-      });*/
+      });
 
     // Default AZs is all in region, but for environment-agnostic stack, max is 2!
     const vpc = new Vpc(this, generateResourceName("vpc"), { maxAzs: 2 });
     const cluster = new Cluster(this, generateResourceName("cluster"), { vpc });
+
+    const apiKeySecret = Secret.fromSecretNameV2(this, generateResourceName("apiKey"), "dev/SpyLogic/ApiKey")
 
     // Create a load-balanced Fargate service and make it public
     new ApplicationLoadBalancedFargateService(
@@ -36,11 +39,14 @@ export class ApiStack extends Stack {
         cpu: 256, // Default is 256
         desiredCount: 1, // Bump this up for prod!
         taskImageOptions: {
-          image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-          /*environment: {
-          NODE_ENV: "development",
-          PORT: "3001",
-        },*/
+          image: ContainerImage.fromDockerImageAsset(dockerImageAsset),
+          environment: {
+            NODE_ENV: "development",
+            PORT: "3001",
+          },
+          secrets: {
+            OPENAI_API_KEY: EnvSecret.fromSecretsManager(apiKeySecret, "OPENAI_API_KEY"),
+          },
         },
         memoryLimitMiB: 512, // Default is 512
         loadBalancerName: generateResourceName("elb"),
