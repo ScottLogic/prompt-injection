cd1021d11c6cb2b167f82a3ffda467086298d8ac
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2023-11-01 14:07:16 +0000

    prettier

--- frontend/src/components/Overlay/LevelsComplete.tsx
+++ frontend/src/components/Overlay/LevelsComplete.tsx
@@ -25,7 +25,7 @@ function LevelsComplete({
         <h1>Congratulations!</h1>
         <div className="content">
           <MissionDialogue dialogueLines={managerDialogue} />
-          <hr/>
+          <hr />
           <p>
             You&apos;ve completed the story mode! You can stay here and continue
             to play with the levels, or you can move onto Sandbox mode where you
