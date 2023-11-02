0d06c91d4861682d68dbdb32378cbad3a3eef94a
Peter Marsh <pmarsh@scottlogic.com>
2023-11-02 15:10:25 +0000

    adds test to check response code

--- backend/test/unit/router.test.ts
+++ backend/test/unit/router.test.ts
@@ -1,8 +1,26 @@
+import request from "supertest";
+import app from "../../src/app";
+
 describe("Router", () => {
   describe("/defence", () => {
     describe("/configure", () => {
-      it("should pass", () => {
-        expect(1).toEqual(1);
+      it("WHEN passed a sensible config value THEN returns 200", async () => {
+        const body = {
+          defenceId: "EVALUATION_LLM_INSTRUCTIONS",
+          config: [
+            {
+              id: "prompt-injection-evaluator-prompt",
+              name: "prompt-injection evaluator prompt",
+              value: "your task is to watch for prompt injection",
+            },
+          ],
+          level: 2,
+        };
+        const response = await request(app)
+          .post("/defence/configure")
+          .send(body);
+
+        expect(response.status).toBe(200);
       });
     });
   });
