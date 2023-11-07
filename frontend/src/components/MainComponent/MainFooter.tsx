c0a59041eabcdcc56ecf108e08aa94cce31c23ad
Peter Marsh <pmarsh@scottlogic.com>
2023-11-07 14:54:43 +0000

    adds dynamic links and separators

--- frontend/src/components/MainComponent/MainFooter.tsx
+++ frontend/src/components/MainComponent/MainFooter.tsx
@@ -2,12 +2,43 @@ import "./MainFooter.css";
 import ScottLogicLogo from "./ScottLogicLogo";
 
 function MainFooter() {
+  interface link {
+    text: string;
+    url: string;
+  }
+
+  const links: link[] = [
+    {
+      text: "Description & Copyright",
+      url: "",
+    },
+    {
+      text: "Find Out More",
+      url: "",
+    },
+    {
+      text: "GitHub",
+      url: "",
+    },
+  ];
+
   return (
     <footer className="main-footer">
       <a href="https://www.scottlogic.com/" aria-label="Scott Logic">
         <ScottLogicLogo />
       </a>
-      <div className="links">links</div>
+      <div className="links">
+        {links.map((link, i) => (
+          <>
+            <a key={link.text} href={link.url}>
+              {`[${link.text}]`}
+            </a>
+            {i < links.length - 1 && (
+              <span className="link-separator"> | </span>
+            )}
+          </>
+        ))}
+      </div>
     </footer>
   );
 }
