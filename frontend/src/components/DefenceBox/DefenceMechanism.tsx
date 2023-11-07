a84315d6063635bad87521cabe94500873ad130e
George Sproston <gsproston@scottlogic.com>
2023-11-07 09:24:42 +0000

    Fix textarea size bug on firefox

--- frontend/src/components/DefenceBox/DefenceMechanism.tsx
+++ frontend/src/components/DefenceBox/DefenceMechanism.tsx
@@ -30,6 +30,7 @@ function DefenceMechanism({
 }) {
   const [isConfigured, setIsConfigured] = useState<boolean>(false);
   const [configValidated, setConfigValidated] = useState<boolean>(true);
+  const [configKey, setConfigKey] = useState<number>(0);
 
   async function resetConfigurationValue(configId: string): Promise<string> {
     const defaultValue = await resetDefenceConfig(defenceDetail.id, configId);
@@ -73,7 +74,14 @@ function DefenceMechanism({
   }
 
   return (
-    <details className="defence-mechanism">
+    <details
+      className="defence-mechanism"
+      onToggle={() => {
+        // re-render the configuration component when detail is toggled
+        // this is to resize the textarea when detail is expanded
+        setConfigKey(configKey + 1);
+      }}
+    >
       <summary>
         <span aria-hidden>{defenceDetail.name}</span>
         <label className="switch">
@@ -88,7 +96,7 @@ function DefenceMechanism({
           <span className="slider round"></span>
         </label>
       </summary>
-      <div className="info-box">
+      <div className="info-box" key={configKey}>
         <p>{defenceDetail.info}</p>
         {showConfigurations &&
           defenceDetail.config.map((config) => {
