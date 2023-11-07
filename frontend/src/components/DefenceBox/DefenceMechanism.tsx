04f8917dce98a5b07d0e284b1ef024a1761bf5c1
George Sproston <103250539+gsproston-scottlogic@users.noreply.github.com>
2023-11-07 14:43:41 +0000

    Fix textarea size bug on firefox (#520)
    * Fix textarea size bug on firefox
    
    * Better key location

--- frontend/src/components/DefenceBox/DefenceMechanism.tsx
+++ frontend/src/components/DefenceBox/DefenceMechanism.tsx
@@ -30,6 +30,7 @@ function DefenceMechanism({
 }) {
   const [isConfigured, setIsConfigured] = useState<boolean>(false);
   const [configValidated, setConfigValidated] = useState<boolean>(true);
+  const [configKey, setConfigKey] = useState<number>(0);
 
   async function resetConfigurationValue(configId: string): Promise<string> {
     const defaultValue = await resetDefenceConfig(defenceDetail.id, configId);
@@ -73,7 +74,14 @@ function DefenceMechanism({
   }
 
   return (
-    <details className="defence-mechanism">
+    <details
+      className="defence-mechanism"
+      onToggle={() => {
+        // re-render the configuration component when detail is toggled
+        // this is to resize the textarea when detail is expanded
+        setConfigKey(configKey + 1);
+      }}
+    >
       <summary>
         <span aria-hidden>{defenceDetail.name}</span>
         <label className="switch">
@@ -94,7 +102,7 @@ function DefenceMechanism({
           defenceDetail.config.map((config) => {
             return (
               <DefenceConfiguration
-                key={config.id}
+                key={config.id + configKey}
                 isActive={defenceDetail.isActive}
                 config={config}
                 setConfigurationValue={setConfigurationValue}
