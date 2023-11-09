c34b3f59b23ce1a41ea4004e33d03198ff3a78a0
Peter Marsh <pmarsh@scottlogic.com>
2023-11-09 13:21:34 +0000

    extend useOverflow to allow for horizontal scrolling

--- frontend/src/hooks/useIsOverflow.ts
+++ frontend/src/hooks/useIsOverflow.ts
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 
-function useIsOverflow(ref: React.MutableRefObject<HTMLInputElement | null>) {
+function useIsOverflow(
+  ref: React.MutableRefObject<HTMLInputElement | null>,
+  direction: "vertical" | "horizontal" = "vertical"
+) {
   const [isOverflow, setIsOverflow] = useState(false);
 
   function checkForOverflow() {
     const { current } = ref;
 
-    const hasOverflow = current
-      ? current.scrollHeight > current.clientHeight
-      : false;
+    if (!current) return;
+
+    const hasOverflow =
+      direction === "vertical"
+        ? current.scrollHeight > current.clientHeight
+        : current.scrollWidth > current.clientWidth;
 
     setIsOverflow(hasOverflow);
   }
