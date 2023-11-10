e3dce54892d355114f30e78dddc673004f06108b
Peter Marsh <118171430+pmarsh-scottlogic@users.noreply.github.com>
2023-11-10 11:36:58 +0000

    509 focusable scrollable areas (#549)
    * adds rel external to scott logic website link
    
    * removes redundant email div
    
    * removes unused classes from sentEmail divs
    
    * converts sentEmail divs to p elements
    
    * apply tabindex if scrolling
    
    * extend useOverflow to allow for horizontal scrolling
    
    * add tabindex to chatboxfeed
    
    * fixes index of handbook pages (so that ids go in order of pages)
    
    * adds dynamic tabindex to handbook pages
    
    * removes padding from handbook content bottom, so that scrollbar only appears when needed
    
    * sets better name for chat box feed container ref
    
    * better variable name for email box container ref
    
    * isOverflow now just returns true if there's any overflow horizontally or vertically
    
    * adds comments to disambiguate useEffects
    
    * fixes ref types
    
    * makes themedTextArea focusable even if turned off. Instead of disableing it we apply readonly

--- frontend/src/components/EmailBox/EmailBox.tsx
+++ frontend/src/components/EmailBox/EmailBox.tsx
@@ -1,15 +1,23 @@
 import "./EmailBox.css";
 import SentEmail from "./SentEmail";
 import { EmailInfo } from "../../models/email";
+import { useRef } from "react";
+import useIsOverflow from "../../hooks/useIsOverflow";
 
 function EmailBox({ emails }: { emails: EmailInfo[] }) {
+  const emailBoxContainer = useRef<HTMLDivElement>(null);
+  const isOverflow = useIsOverflow(emailBoxContainer);
+
   return (
-    <div className="email-box">
-      <div className="email-box-feed">
-        {[...emails].reverse().map((email, index) => {
-          return <SentEmail emailDetails={email} key={index} />;
-        })}
-      </div>
+    <div
+      className="email-box"
+      ref={emailBoxContainer}
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+      tabIndex={isOverflow ? 0 : -1}
+    >
+      {[...emails].reverse().map((email, index) => {
+        return <SentEmail emailDetails={email} key={index} />;
+      })}
     </div>
   );
 }
--- frontend/src/models/handbook.ts
+++ frontend/src/models/handbook.ts
@@ -1,13 +1,13 @@
 enum HANDBOOK_PAGES {
   ATTACKS,
-  GLOSSARY,
   SYSTEM_ROLE,
+  GLOSSARY,
 }
 
 const handbookPageNames: { [key in HANDBOOK_PAGES]: string } = {
   [HANDBOOK_PAGES.ATTACKS]: "Attacks",
-  [HANDBOOK_PAGES.GLOSSARY]: "Glossary",
   [HANDBOOK_PAGES.SYSTEM_ROLE]: "System Role",
+  [HANDBOOK_PAGES.GLOSSARY]: "Glossary",
 };
 
 export { HANDBOOK_PAGES, handbookPageNames };
