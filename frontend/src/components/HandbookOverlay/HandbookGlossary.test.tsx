df2b64cfe8d01609c3754e8267fcdd8fd308f401
Heather Logan (She/Her) <hlogan@scottlogic.com>
2023-11-22 11:50:23 +0000

    unit tests for handbook page components

--- frontend/src/components/HandbookOverlay/HandbookGlossary.test.tsx
+++ frontend/src/components/HandbookOverlay/HandbookGlossary.test.tsx
@@ -1,13 +1,20 @@
+import { GLOSSARY } from "@src/Glossary";
+import { LEVEL_NAMES } from "@src/models/level";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 
 import HandbookGlossary from "./HandbookGlossary";
 
-import { GLOSSARY } from "@src/Glossary";
-import { LEVEL_NAMES } from "@src/models/level";
-
 describe("HandbookGlossary component tests", () => {
-  test("renders all glossary entries with ARIA roles", () => {
+  test("renders a header and no glossary entries", () => {
+    render(<HandbookGlossary currentLevel={LEVEL_NAMES.LEVEL_1} />);
+
+    expect(screen.getByText("Glossary")).toBeInTheDocument();
+    // make sure no terms are rendered
+    expect(screen.queryAllByRole("term")).toHaveLength(0);
+  });
+
+  test("renders all glossary entries with ARIA roles for level 2-3", () => {
     render(<HandbookGlossary currentLevel={LEVEL_NAMES.LEVEL_3} />);
 
     const glossaryTerms = screen
