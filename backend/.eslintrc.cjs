7b8d389d43d3c047132913c33cdfa234eab3dad1
Peter Marsh <pmarsh@scottlogic.com>
2023-11-23 17:16:19 +0000

    adds es lint ordering rules back in

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,47 +1,55 @@
 /* eslint-env node */
 module.exports = {
-  root: true,
-  settings: {
-    "import/resolver": {
-      alias: {
-        map: [["@src", "./src"]],
-        extensions: [".ts", ".js", ".jsx", ".tsx", ".json", ".css"],
-      },
-    },
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["./tsconfig.json", "./test/tsconfig.json"],
-  },
-  plugins: ["@typescript-eslint"],
-  ignorePatterns: ["build", "coverage", "node_modules"],
-  rules: {
-    eqeqeq: "error",
-    "func-style": ["error", "declaration"],
-    "object-shorthand": "error",
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["../*"],
-      },
-    ],
-    "prefer-template": "error",
+	root: true,
+	settings: {
+		'import/resolver': {
+			alias: {
+				map: [['@src', './src']],
+				extensions: ['.ts', '.js', '.jsx', '.tsx', '.json', '.css'],
+			},
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['./tsconfig.json', './test/tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['build', 'coverage', 'node_modules'],
+	rules: {
+		eqeqeq: 'error',
+		'func-style': ['error', 'declaration'],
+		'object-shorthand': 'error',
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['../*'],
+			},
+		],
+		'prefer-template': 'error',
 
-    "@typescript-eslint/init-declarations": "error",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: false,
-      },
-    ],
-    "@typescript-eslint/unbound-method": ["error", { ignoreStatic: true }],
-  },
+		'@typescript-eslint/init-declarations': 'error',
+		'@typescript-eslint/no-misused-promises': [
+			'error',
+			{
+				checksVoidReturn: false,
+			},
+		],
+		'@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
+		'import/order': [
+			'error',
+			{
+				alphabetize: { order: 'asc' },
+				'newlines-between': 'always',
+				warnOnUnassignedImports: true,
+			},
+		],
+	},
 };
--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,64 +1,72 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  settings: {
-    react: {
-      version: "detect",
-    },
-    "import/resolver": {
-      alias: {
-        map: [["@src", "./src"]],
-        extensions: [".ts", ".js", ".jsx", ".tsx", ".json", ".css"],
-      },
-    },
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:jest-dom/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ["react-refresh", "jsx-a11y", "jest-dom"],
-  rules: {
-    "@typescript-eslint/init-declarations": "error",
+	root: true,
+	env: { browser: true, es2020: true },
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': {
+			alias: {
+				map: [['@src', './src']],
+				extensions: ['.ts', '.js', '.jsx', '.tsx', '.json', '.css'],
+			},
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:jest-dom/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['react-refresh', 'jsx-a11y', 'jest-dom'],
+	rules: {
+		'@typescript-eslint/init-declarations': 'error',
 
-    eqeqeq: "error",
-    "func-style": ["error", "declaration"],
-    "object-shorthand": "error",
-    "prefer-template": "error",
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["../*"],
-      },
-    ],
+		eqeqeq: 'error',
+		'func-style': ['error', 'declaration'],
+		'object-shorthand': 'error',
+		'prefer-template': 'error',
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['../*'],
+			},
+		],
 
-    // turn this off to allow the use of useEffect on mount
-    "react-hooks/exhaustive-deps": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-  overrides: [
-    {
-      files: ["*.test.ts{,x}"],
-      rules: {
-        "@typescript-eslint/no-empty-function": "off",
-      },
-    },
-  ],
+		// turn this off to allow the use of useEffect on mount
+		'react-hooks/exhaustive-deps': 'off',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'import/order': [
+			'error',
+			{
+				alphabetize: { order: 'asc' },
+				'newlines-between': 'always',
+				warnOnUnassignedImports: true,
+			},
+		],
+	},
+	overrides: [
+		{
+			files: ['*.test.ts{,x}'],
+			rules: {
+				'@typescript-eslint/no-empty-function': 'off',
+			},
+		},
+	],
 };
