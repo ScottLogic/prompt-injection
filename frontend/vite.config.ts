512ad8467ee0afe8c6e434d5c015b8df069ccc70
Peter Marsh <pmarsh@scottlogic.com>
2023-11-24 15:54:45 +0000

    lint frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
-import * as path from 'path';
-
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
