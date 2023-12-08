2843d19595cecb8e60ceabae0914faedb37bf74c
Heather Logan (She/Her) <hlogan@scottlogic.com>
2023-12-08 14:49:07 +0000

    do not reset sandbox defence configs on reset

--- backend/src/controller/resetController.ts
+++ backend/src/controller/resetController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 
-import { levelsInitialState } from '@src/models/level';
+import { LEVEL_NAMES, levelsInitialState } from '@src/models/level';
 
 function handleResetProgress(req: Request, res: Response) {
-	console.debug('Resetting progress for all levels');
-	req.session.levelState = levelsInitialState;
+	console.debug('Resetting progress for all levels', req.session.levelState);
+	// keep track of sandbox state to preserve defences
+	const sandboxDefenceState =
+		req.session.levelState[LEVEL_NAMES.SANDBOX].defences;
+	req.session.levelState = { ...levelsInitialState };
+	req.session.levelState[LEVEL_NAMES.SANDBOX].defences = sandboxDefenceState;
 	res.send(req.session.levelState);
 }
 
