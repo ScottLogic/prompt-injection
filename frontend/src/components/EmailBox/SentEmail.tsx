bab7b7a892433079d01771d20a62234436276c47
Peter Marsh <pmarsh@scottlogic.com>
2023-12-08 13:09:58 +0000

    styling changes

--- frontend/src/components/EmailBox/SentEmail.tsx
+++ frontend/src/components/EmailBox/SentEmail.tsx
@@ -5,14 +5,17 @@ import './SentEmail.css';
 function SentEmail({ emailDetails }: { emailDetails: EmailInfo }) {
 	return (
 		<div className="sent-email">
-			<p>
-				<b>To:</b> {emailDetails.address}
-			</p>
-			<p>
-				<b>Subject:</b> {emailDetails.subject}
-			</p>
-			<hr />
-			<p>{emailDetails.body}</p>
+			<div className="sent-email-title">E-Mail</div>
+			<div className="sent-email-main">
+				<p>
+					<b>To:</b> {emailDetails.address}
+				</p>
+				<p>
+					<b>Subject:</b> {emailDetails.subject}
+				</p>
+				<hr />
+				<p>{emailDetails.body}</p>
+			</div>
 		</div>
 	);
 }
