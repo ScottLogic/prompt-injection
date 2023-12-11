c97eabba721404d86d2a651fe6018b777c6247d1
Chris Wilton-Magras <chriswilty@zoho.com>
2023-12-11 11:56:00 +0000

    Addressed PR comments, more colours into theme

--- frontend/src/components/ChatBox/ChatBoxMessage.tsx
+++ frontend/src/components/ChatBox/ChatBoxMessage.tsx
@@ -20,7 +20,6 @@ function ChatBoxMessage({ message }: { message: ChatMessage }) {
 			}
 			lang="en"
 		>
-			{message.type === CHAT_MESSAGE_TYPE.USER && <b>Input: </b>}
 			{message.type === CHAT_MESSAGE_TYPE.USER_TRANSFORMED && <b>Edited: </b>}
 			{message.type === CHAT_MESSAGE_TYPE.LEVEL_INFO && (
 				<p className="level-info-header">Information</p>
