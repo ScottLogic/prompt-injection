f6d7f32579ed74cfec6fa32c37d2e6f7f196f879
Peter Marsh <pmarsh@scottlogic.com>
2023-12-13 15:35:31 +0000

    change circle colour for bot

--- frontend/src/components/ChatBox/Avatar.tsx
+++ frontend/src/components/ChatBox/Avatar.tsx
@@ -1,11 +1,17 @@
+import { clsx } from 'clsx';
+
 import botAvatarDefault from '@src/assets/images/BotAvatarDefault.svg';
 import userAvatar from '@src/assets/images/UserAvatar.svg';
 
 import './Avatar.css';
 
 function Avatar({ type }: { type: 'user' | 'bot' }) {
+	const avatarClass = clsx(
+		'avatar-circle',
+		type === 'user' ? 'avatar-circle-user' : 'avatar-circle-bot'
+	);
 	return (
-		<div className="avatar-circle">
+		<div className={avatarClass}>
 			<img src={type === 'user' ? userAvatar : botAvatarDefault} alt="" />
 		</div>
 	);
