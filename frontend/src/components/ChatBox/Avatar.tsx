975f6d2ec40b57efb22a583f1a1ae586e172eeb4
Peter Marsh <pmarsh@scottlogic.com>
2023-12-13 15:12:37 +0000

    adds pictures to avater circles

--- frontend/src/components/ChatBox/Avatar.tsx
+++ frontend/src/components/ChatBox/Avatar.tsx
@@ -1,7 +1,14 @@
+import botAvatarDefault from '@src/assets/images/BotAvatarDefault.svg';
+import userAvatar from '@src/assets/images/UserAvatar.svg';
+
 import './Avatar.css';
 
-function Avatar() {
-	return <div className="circle"></div>;
+function Avatar({ type }: { type: 'user' | 'bot' }) {
+	return (
+		<div className="avatar-circle">
+			<img src={type === 'user' ? userAvatar : botAvatarDefault} alt="" />
+		</div>
+	);
 }
 
 export default Avatar;
