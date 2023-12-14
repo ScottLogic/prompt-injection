566668764ce6c81d631e293685b4ebbd3f7f571f
Chris Wilton-Magras <chriswilty@zoho.com>
2023-12-14 17:18:39 +0000

    658: Sporadic UI test failures (#666)
    * Clean up requests on unmount (WIP)
    
    * Try to fix sporadic UI test failures
    
    * Fix post-merge problem

--- frontend/src/service/documentService.ts
+++ frontend/src/service/documentService.ts
@@ -2,9 +2,9 @@ import { DocumentMeta } from '@src/models/document';
 
 import { getBackendUrl, sendRequest } from './backendService';
 
-async function getDocumentMetas(): Promise<DocumentMeta[]> {
+async function getDocumentMetas(signal?: AbortSignal): Promise<DocumentMeta[]> {
 	const path = 'documents';
-	const response = await sendRequest(path, 'GET');
+	const response = await sendRequest(path, { method: 'GET', signal });
 	let documentMetas = (await response.json()) as DocumentMeta[];
 	documentMetas = documentMetas.map((documentMeta) => {
 		return {
