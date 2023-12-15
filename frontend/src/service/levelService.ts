dfff3e14ce4caa5db6920a5a8b5e233aeaf51d4c
George Sproston <103250539+gsproston-scottlogic@users.noreply.github.com>
2023-12-15 09:58:21 +0000

    Fixed build (#670)
    * Fixed build
    
    * Removing body from reset level API request

--- frontend/src/service/levelService.ts
+++ frontend/src/service/levelService.ts
@@ -1,14 +1,12 @@
 import { sendRequest } from './backendService';
 
 async function resetAllLevelProgress(): Promise<boolean> {
-	const response = await sendRequest(
-		`/reset`,
-		'POST',
-		{
+	const response = await sendRequest(`/reset`, {
+		method: 'POST',
+		headers: {
 			'Content-Type': 'application/json',
 		},
-		JSON.stringify({})
-	);
+	});
 	return response.status === 200;
 }
 
