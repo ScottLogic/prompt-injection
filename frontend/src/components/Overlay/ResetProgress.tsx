8850121e527d9d3e267f2ee2080e45259b0c6916
George Sproston <gsproston@scottlogic.com>
2023-12-21 17:05:48 +0000

    New OverlayChoice component

--- frontend/src/components/Overlay/ResetProgress.tsx
+++ frontend/src/components/Overlay/ResetProgress.tsx
@@ -1,8 +1,4 @@
-import OverlayChoiceButtons from '@src/components/ThemedButtons/OverlayChoiceButtons';
-
-import Overlay from './Overlay';
-
-import './ResetProgress.css';
+import OverlayChoice from './OverlayChoice';
 
 function ResetProgressOverlay({
 	resetProgress,
@@ -12,29 +8,30 @@ function ResetProgressOverlay({
 	closeOverlay: () => void;
 }) {
 	return (
-		<Overlay closeOverlay={closeOverlay}>
-			<div className="reset-progress">
-				<h1> Reset all progress </h1>
-				<p>
-					{`Warning! This will reset all your progress in the levels and sandbox mode. 
+		<OverlayChoice
+			button1={{
+				children: 'Reset',
+				onClick: () => {
+					void resetProgress();
+				},
+			}}
+			button2={{
+				children: 'Cancel',
+				onClick: closeOverlay,
+			}}
+			content={
+				<div className="reset-progress">
+					<h1> Reset all progress </h1>
+					<p>
+						{`Warning! This will reset all your progress in the levels and sandbox mode. 
 							This includes all your conversation history and sent emails. 
 							However any configurations you have made to defences in sandbox mode will not be lost.
 							Are you sure you want to do this?`}
-				</p>
-				<OverlayChoiceButtons
-					button1={{
-						children: 'Reset',
-						onClick: () => {
-							void resetProgress();
-						},
-					}}
-					button2={{
-						children: 'Cancel',
-						onClick: closeOverlay,
-					}}
-				/>
-			</div>
-		</Overlay>
+					</p>
+				</div>
+			}
+			closeOverlay={closeOverlay}
+		/>
 	);
 }
 
