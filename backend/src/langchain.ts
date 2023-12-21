4b285e15f21789a4575ec593941b657390d0f9a1
George Sproston <gsproston@scottlogic.com>
2023-12-21 13:27:53 +0000

    No longer exporting makePromptTemplate
    Also removed unit tests, as this method was being covered in the integration tests already

--- backend/src/langchain.ts
+++ backend/src/langchain.ts
@@ -104,7 +104,7 @@ function initQAModel(level: LEVEL_NAMES, Prompt: string) {
 		prompt: promptTemplate,
 	});
 }
-// initialise the prompt evaluation model
+
 function initPromptEvaluationModel(configPromptEvaluationPrompt: string) {
 	const openAIApiKey = getOpenAIKey();
 	// use gpt-4 if avaliable to apiKey
@@ -209,9 +209,4 @@ function formatEvaluationOutput(response: string) {
 }
 
 export const setVectorisedDocuments = vectorisedDocuments.set;
-export {
-	queryDocuments,
-	queryPromptEvaluationModel,
-	initDocumentVectors,
-	makePromptTemplate,
-};
+export { queryDocuments, queryPromptEvaluationModel, initDocumentVectors };
