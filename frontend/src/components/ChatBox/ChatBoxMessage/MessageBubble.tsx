ac358f109f9d1cad3b9adfb3e57c1186c8780548
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2023-12-21 13:59:22 +0000

    restored message bubble

--- frontend/src/components/ChatBox/ChatBoxMessage/MessageBubble.tsx
+++ frontend/src/components/ChatBox/ChatBoxMessage/MessageBubble.tsx
@@ -34,34 +34,16 @@ function MessageBubble({
 		positionClassName
 	);
 
-	const messageAuthor =
-		message.type === CHAT_MESSAGE_TYPE.LEVEL_INFO
-			? ''
-			: message.type === CHAT_MESSAGE_TYPE.USER
-			? 'You said: '
-			: message.type === CHAT_MESSAGE_TYPE.USER_TRANSFORMED
-			? 'Your message transformed by XML tagging: '
-			: message.type === CHAT_MESSAGE_TYPE.ERROR_MSG
-			? 'Error message: '
-			: message.type === CHAT_MESSAGE_TYPE.BOT
-			? 'ScottBrewBot said:  '
-			: 'ScottBrewBot said: ';
-
 	return (
-		// eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-		<section className={className} tabIndex={0}>
+		<div className={className} lang="en">
 			{message.type === CHAT_MESSAGE_TYPE.USER_TRANSFORMED && (
 				<b>Transformed: </b>
 			)}
 			{message.type === CHAT_MESSAGE_TYPE.LEVEL_INFO && (
-				<>
-					<p className="level-info-header">Information</p>
-					<span className="visually-hidden"> message</span>
-				</>
+				<p className="level-info-header">Information</p>
 			)}
-			<span className="visually-hidden">{messageAuthor}</span>
 			{message.message}
-		</section>
+		</div>
 	);
 }
 
