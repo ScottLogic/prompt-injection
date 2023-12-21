ee9605b3d39e6c9187cf47ec425f765be673c82e
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2023-12-21 14:15:11 +0000

    changed hidden header

--- frontend/src/components/ControlPanel/ControlPanel.tsx
+++ frontend/src/components/ControlPanel/ControlPanel.tsx
@@ -55,7 +55,7 @@ function ControlPanel({
 			{(currentLevel === LEVEL_NAMES.LEVEL_3 ||
 				currentLevel === LEVEL_NAMES.SANDBOX) && (
 				<>
-					<h2 className="visually-hidden">Chatbot Configurations</h2>
+					<h2 className="visually-hidden">ScottBrew System Access</h2>
 					<details className="control-collapsible-section">
 						<summary className="control-collapsible-section-header">
 							Defence Configuration
