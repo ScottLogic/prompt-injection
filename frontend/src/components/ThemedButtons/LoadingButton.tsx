d7499d1aec9783b86f4d8e66febc74dccaa7fea8
George Sproston <103250539+gsproston-scottlogic@users.noreply.github.com>
2024-01-03 16:10:38 +0000

    598 add tooltips to disabled buttons (#733)
    * Added tooltip to LoadingButton
    
    * Added tooltip to LevelButton
    
    * Better tooltips for LoadingButton

--- frontend/src/components/ThemedButtons/LoadingButton.tsx
+++ frontend/src/components/ThemedButtons/LoadingButton.tsx
@@ -7,15 +7,19 @@ import './LoadingButton.css';
 function LoadingButton({
 	children,
 	isLoading = false,
+	loadingTooltip,
 	onClick,
 }: {
 	children: React.ReactNode;
 	isLoading?: boolean;
+	loadingTooltip?: string;
 	onClick: () => void;
 }) {
 	return (
 		<ThemedButton
 			ariaDisabled={isLoading}
+			// tooltip is shown when button is disabled
+			title={isLoading ? loadingTooltip : undefined}
 			className="loading-button"
 			onClick={onClick}
 		>
