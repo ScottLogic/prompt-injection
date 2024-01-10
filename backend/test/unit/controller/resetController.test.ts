c7961a3a0fe3035b13896a5b24548d7368978cd1
Heather Logan <118981273+heatherlogan-scottlogic@users.noreply.github.com>
2024-01-10 09:52:25 +0000

    710 reset defences for sandbox and reword modal warning (#739)
    * reset defences for sandbox and reword modal warning
    
    * rename test suite

--- backend/test/unit/controller/resetController.test.ts
+++ backend/test/unit/controller/resetController.test.ts
@@ -9,7 +9,7 @@ import {
 } from '@src/models/chat';
 import { DEFENCE_ID, Defence, DefenceConfigItem } from '@src/models/defence';
 import { EmailInfo } from '@src/models/email';
-import { LEVEL_NAMES, LevelState } from '@src/models/level';
+import { LEVEL_NAMES, LevelState, levelsInitialState } from '@src/models/level';
 
 declare module 'express-session' {
 	interface Session {
@@ -49,7 +49,7 @@ function createLevelObject(
 	return obj;
 }
 
-describe('handleResetAllDefences unit tests', () => {
+describe('handleResetProgress unit tests', () => {
 	test('GIVEN a chat history THEN should reset all chatHistory for all levels', () => {
 		const mockChatHistory: ChatHistoryMessage[] = [
 			{
@@ -66,32 +66,27 @@ describe('handleResetAllDefences unit tests', () => {
 			},
 		} as unknown as Request;
 
-		// empty chat history
-		const expectedLevelState = createLevelObject('chatHistory', []);
-
 		const res = responseMock();
 		handleResetProgress(reqWithChatHistory, res);
-		expect(res.send).toHaveBeenCalledWith(expectedLevelState);
+		expect(res.send).toHaveBeenCalledWith(levelsInitialState);
 	});
 
 	test('GIVEN sent emails THEN should reset emails for all levels', () => {
 		const mockSentEmails: EmailInfo[] = [
 			{ address: 'bob@example.com', subject: 'test', body: 'this is a test' },
 		];
-
 		const reqWithSentEmails = {
 			session: {
 				levelState: createLevelObject('sentEmails', mockSentEmails),
 			},
 		} as unknown as Request;
 
-		const expectedLevelState = createLevelObject('sentEmails', []);
 		const res = responseMock();
 		handleResetProgress(reqWithSentEmails, res);
-		expect(res.send).toHaveBeenCalledWith(expectedLevelState);
+		expect(res.send).toHaveBeenCalledWith(levelsInitialState);
 	});
 
-	test('GIVEN defences THEN should reset defences for levels but not sandbox', () => {
+	test('GIVEN defences THEN should reset defences for levels', () => {
 		function configureAndActivateDefence(
 			id: DEFENCE_ID,
 			defences: Defence[],
@@ -112,33 +107,14 @@ describe('handleResetAllDefences unit tests', () => {
 				},
 			]
 		);
-
 		const reqWithDefences = {
 			session: {
 				levelState: createLevelObject('defences', mockDefences),
 			},
 		} as unknown as Request;
 
-		const expectedLevelState: Record<string, LevelState> = createLevelObject(
-			'defences',
-			defaultDefences
-		);
-
-		// update sandbox as this should not be reset
-		const resetSandbox = configureAndActivateDefence(
-			DEFENCE_ID.CHARACTER_LIMIT,
-			expectedLevelState[LEVEL_NAMES.SANDBOX].defences,
-			[
-				{
-					id: 'MAX_MESSAGE_LENGTH',
-					value: '10',
-				},
-			]
-		);
-		expectedLevelState[LEVEL_NAMES.SANDBOX].defences = resetSandbox;
-
 		const res = responseMock();
 		handleResetProgress(reqWithDefences, res);
-		expect(res.send).toHaveBeenCalledWith(expectedLevelState);
+		expect(res.send).toHaveBeenCalledWith(levelsInitialState);
 	});
 });
