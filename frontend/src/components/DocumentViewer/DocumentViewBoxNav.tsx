ce8d3fcc482c02acdf99a76da50d27508cce75ed
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2024-02-02 11:33:43 +0000

    border colour of doc viewer changed

---
+++ frontend/src/components/DocumentViewer/DocumentViewBoxNav.tsx
@@ -0,0 +1,44 @@
+import ThemedButton from '@src/components/ThemedButtons/ThemedButton';
+
+import './DocumentViewBoxNav.css';
+
+interface DocumentViewBoxNavProps {
+	documentIndex: number;
+	documentName: string;
+	numberOfDocuments: number;
+	onPrevious: () => void;
+	onNext: () => void;
+}
+
+function DocumentViewBoxNav({
+	documentIndex,
+	documentName,
+	numberOfDocuments,
+	onPrevious,
+	onNext,
+}: DocumentViewBoxNavProps) {
+	const documentNumber = `${documentIndex + 1} out of ${numberOfDocuments}`;
+
+	return (
+		<div className="view-documents-nav">
+			<div className="nav">
+				<nav  className="button-container">
+					<ThemedButton onClick={onPrevious} ariaDisabled={documentIndex <= 0}>
+						<i aria-hidden>◄</i>&nbsp;previous document
+					</ThemedButton>
+					<div className="info">{documentNumber}</div>
+					<ThemedButton
+						onClick={onNext}
+						ariaDisabled={documentIndex >= numberOfDocuments - 1}
+					>
+						next document&nbsp;<i aria-hidden>►</i>
+					</ThemedButton>
+				</nav>
+			</div>
+			<p className="info">{documentName}</p>
+		</div>
+	);
+}
+
+export default DocumentViewBoxNav;
+export type { DocumentViewBoxNavProps };
