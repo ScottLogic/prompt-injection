1e62b9a01dc3b2a397d9afbc6ecda010210b7534
Chris Wilton-Magras <chriswilty@zoho.com>
2024-02-02 09:08:13 +0000

    Fix broken tests

--- backend/src/document.ts
+++ backend/src/document.ts
@@ -1,9 +1,9 @@
-import * as fs from 'fs';
 import { CSVLoader } from 'langchain/document_loaders/fs/csv';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import * as fs from 'node:fs';
 
 import { DocumentMeta } from './models/document';
 import { LEVEL_NAMES } from './models/level';
