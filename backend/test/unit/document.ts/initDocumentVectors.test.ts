6c0a83148134bac9919e1d152769780687f1a4c0
Peter Marsh <118171430+pmarsh-scottlogic@users.noreply.github.com>
2024-02-06 14:26:48 +0000

    596 fix problems with integrationlangchaintest (#804)

---
+++ backend/test/unit/document.ts/initDocumentVectors.test.ts
@@ -0,0 +1,46 @@
+import { test, jest, expect, beforeAll, afterEach } from '@jest/globals';
+import { Document } from 'langchain/document';
+
+import { initDocumentVectors } from '@src/document';
+
+const mockLoader =
+	jest.fn<() => Promise<Document<Record<string, unknown>>[]>>();
+const mockSplitDocuments = jest.fn<() => Promise<unknown>>();
+
+// mock DirectoryLoader
+jest.mock('langchain/document_loaders/fs/directory', () => {
+	return {
+		DirectoryLoader: jest.fn().mockImplementation(() => {
+			return {
+				load: mockLoader,
+			};
+		}),
+	};
+});
+
+// mock RecursiveCharacterTextSplitter
+jest.mock('langchain/text_splitter', () => {
+	return {
+		RecursiveCharacterTextSplitter: jest.fn().mockImplementation(() => {
+			return {
+				splitDocuments: mockSplitDocuments,
+			};
+		}),
+	};
+});
+
+beforeAll(() => {
+	mockLoader.mockResolvedValue([]);
+});
+
+afterEach(() => {
+	mockLoader.mockClear();
+});
+
+test('GIVEN application WHEN application starts THEN document vectors are loaded for all levels', async () => {
+	await initDocumentVectors();
+
+	const numberOfCalls = 4 + 1; // number of levels + common
+	expect(mockLoader).toHaveBeenCalledTimes(numberOfCalls);
+	expect(mockSplitDocuments).toHaveBeenCalledTimes(numberOfCalls);
+});
