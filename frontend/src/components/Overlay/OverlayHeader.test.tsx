cf12df415f73d7b145488979883fd57ad161fa90
dhinrichs-scottlogic <125262707+dhinrichs-scottlogic@users.noreply.github.com>
2024-02-07 09:19:12 +0000

    782 added unit test

--- frontend/src/components/Overlay/OverlayHeader.test.tsx
+++ frontend/src/components/Overlay/OverlayHeader.test.tsx
@@ -6,15 +6,19 @@ import OverlayHeader from './OverlayHeader';
 const mockCloseOverlay = vi.fn();
 
 describe('Overlay Header component', () => {
-    test('renders the heading text passed in', () => {
-        render(<OverlayHeader heading="Test Header" closeOverlay={mockCloseOverlay}/>)
-        
-        expect(screen.getByText('Test Header')).toBeInTheDocument();
-    })
+	test('renders the heading text passed in', () => {
+		render(
+			<OverlayHeader heading="Test Header" closeOverlay={mockCloseOverlay} />
+		);
 
-    test('renders close button', () => {
-        render(<OverlayHeader heading="Test Header" closeOverlay={mockCloseOverlay}/>)
-        
-        expect(screen.getByText('close')).toBeInTheDocument();
-    })
-})
+		expect(screen.getByText('Test Header')).toBeInTheDocument();
+	});
+
+	test('renders close button', () => {
+		render(
+			<OverlayHeader heading="Test Header" closeOverlay={mockCloseOverlay} />
+		);
+
+		expect(screen.getByText('close')).toBeInTheDocument();
+	});
+});
