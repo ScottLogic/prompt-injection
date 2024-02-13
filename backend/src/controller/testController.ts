b47cd74b311e9e0c0fc98909b6244fb5acb2ca77
kclark <kclark@scottlogic.com>
2024-02-13 11:12:14 +0000

    formatting

--- backend/src/controller/testController.ts
+++ backend/src/controller/testController.ts
@@ -4,11 +4,10 @@ import { handleAddInfoToChatHistory } from '@src/controller/chatController';
 import { OpenAiAddInfoToChatHistoryRequest } from '@src/models/api/OpenAiAddInfoToChatHistoryRequest';
 
 function handleTest(req: OpenAiAddInfoToChatHistoryRequest, res: Response) {
-    let num = 0
-    for(let x=0; x<= 1000000; x++)
-    {
-        num = num++;
-    }
-    handleAddInfoToChatHistory(req, res);
+	let num = 0;
+	for (let x = 0; x <= 1000000; x++) {
+		num = num++;
+	}
+	handleAddInfoToChatHistory(req, res);
 }
 export { handleTest };
