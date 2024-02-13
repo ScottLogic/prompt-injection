78d67d6f9d4bcd93426fcafdac5108f292cc1103
kclark <kclark@scottlogic.com>
2024-02-13 10:08:48 +0000

    linting/formatting

--- backend/src/controller/testController.ts
+++ backend/src/controller/testController.ts
@@ -3,17 +3,15 @@ import { Response } from 'express';
 import { OpenAiChatRequest } from '@src/models/api/OpenAiChatRequest';
 
 function handleTest(req: OpenAiChatRequest, res: Response) {
-    const { message, currentLevel } = req.body;
+	const { message, currentLevel } = req.body;
 
-    if (!message || currentLevel === undefined) {
+	if (!message || currentLevel === undefined) {
 		console.log('Missing or empty message or level');
-        res.send(400);	
+		res.send(400);
 		return;
+	} else {
+		console.log('Dummy test endpoint');
+		res.send(200);
 	}
-    else{
-        console.log('Dummy test endpoint');
-        res.send(200);
-    }
-
 }
 export { handleTest };
