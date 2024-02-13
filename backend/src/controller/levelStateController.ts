b1fcfa7e51925a68fa238b9b92996194977fa48a
Peter Marsh <pmarsh@scottlogic.com>
2024-02-13 14:01:37 +0000

    fill out handleGetLevelState

--- backend/src/controller/levelStateController.ts
+++ backend/src/controller/levelStateController.ts
@@ -1,7 +1,32 @@
 import { Response } from 'express';
 
-function handleGetLevelState(_req: unknown, res: Response) {
-	res.send();
+import { LevelStateRequest } from '@src/models/api/LevelStateRequest';
+import { LEVEL_NAMES } from '@src/models/level';
+import { getValidOpenAIModelsList } from '@src/openai';
+import {
+	systemRoleLevel1,
+	systemRoleLevel2,
+	systemRoleLevel3,
+} from '@src/promptTemplates';
+
+function handleGetLevelState(req: LevelStateRequest, res: Response) {
+	const level = req.query.level;
+
+	// put level into the URL to ensure type safety
+
+	const systemRoles = [
+		{ level: LEVEL_NAMES.LEVEL_1, systemRole: systemRoleLevel1 },
+		{ level: LEVEL_NAMES.LEVEL_2, systemRole: systemRoleLevel2 },
+		{ level: LEVEL_NAMES.LEVEL_3, systemRole: systemRoleLevel3 },
+	];
+
+	res.send({
+		emails: req.session.levelState[level].sentEmails,
+		history: req.session.levelState[level].chatHistory,
+		defences: req.session.levelState[level].defences,
+		validModels: getValidOpenAIModelsList(),
+		systemRoles,
+	});
 }
 
 export { handleGetLevelState };
---
+++ backend/src/models/api/LevelStateRequest.ts
@@ -0,0 +1,21 @@
+import { Request } from 'express';
+
+import { ChatMessage } from '@src/models/chatMessage';
+import { Defence } from '@src/models/defence';
+import { EmailInfo } from '@src/models/email';
+import { LEVEL_NAMES } from '@src/models/level';
+
+export type LevelStateRequest = Request<
+	never,
+	{
+		emails: EmailInfo[];
+		history: ChatMessage[];
+		defences: Defence[];
+		validModels: string[];
+		systemRoles: string[];
+	},
+	never,
+	{
+		level: LEVEL_NAMES;
+	}
+>;
