a1dec0799a469ef85029514a66e470d3634caf20
Peter Marsh <pmarsh@scottlogic.com>
2024-02-22 11:07:21 +0000

    adds load level service to frontend

---
+++ frontend/src/service/levelService.ts
@@ -0,0 +1,23 @@
+import { LoadLevelResponse } from '@src/models/combined';
+
+import { sendRequest } from './backendService';
+import { getChatMessagesFromDTOResponse } from './chatService';
+import { getDefencesFromDTOs } from './defenceService';
+
+const PATH = 'level/';
+
+async function loadLevel(level: number) {
+	const response = await sendRequest(`${PATH}?level=${level}`, {
+		method: 'GET',
+	});
+	const { defences, emails, chatHistory } =
+		(await response.json()) as LoadLevelResponse;
+
+	return {
+		emails,
+		history: getChatMessagesFromDTOResponse(chatHistory),
+		defences: getDefencesFromDTOs(defences),
+	};
+}
+
+export { loadLevel };
