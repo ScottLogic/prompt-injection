2774b9cf6c8c2675107b1567f32c16d045346539
Peter Marsh <pmarsh@scottlogic.com>
2024-02-22 11:14:05 +0000

    adds levelSErvice to servvice index

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -2,6 +2,7 @@ import * as chatService from './chatService';
 import * as defenceService from './defenceService';
 import * as documentService from './documentService';
 import * as emailService from './emailService';
+import * as levelService from './levelService';
 import * as resetService from './resetService';
 import * as startService from './startService';
 
@@ -12,4 +13,5 @@ export {
 	emailService,
 	resetService,
 	startService,
+	levelService,
 };
