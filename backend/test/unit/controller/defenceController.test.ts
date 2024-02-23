7e86ae42089e78129edda9f52a4b35bf8c851a26
Peter Marsh <pmarsh@scottlogic.com>
2024-02-23 15:12:41 +0000

    fixes tests

--- backend/test/unit/controller/defenceController.test.ts
+++ backend/test/unit/controller/defenceController.test.ts
@@ -19,7 +19,7 @@ declare module 'express-session' {
 	interface LevelState {
 		level: LEVEL_NAMES;
 		chatHistory: ChatMessage[];
-		defences: Defence[];
+		defences?: Defence[];
 		sentEmails: EmailInfo[];
 	}
 }
