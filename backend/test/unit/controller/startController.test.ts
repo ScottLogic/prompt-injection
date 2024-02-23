3d5c3639d564a73d4e30ef6736b9a7a1a3ac115e
Peter Marsh <pmarsh@scottlogic.com>
2024-02-23 13:16:28 +0000

    adds getStart test

--- backend/test/unit/controller/startController.test.ts
+++ backend/test/unit/controller/startController.test.ts
@@ -2,6 +2,32 @@ import { expect, test, jest } from '@jest/globals';
 import { Response } from 'express';
 
 import { handleStart } from '@src/controller/startController';
+import { StartGetRequest } from '@src/models/api/StartGetRequest';
+import { ChatModel } from '@src/models/chat';
+import { ChatMessage } from '@src/models/chatMessage';
+import { Defence } from '@src/models/defence';
+import { EmailInfo } from '@src/models/email';
+import { LEVEL_NAMES } from '@src/models/level';
+
+declare module 'express-session' {
+	interface Session {
+		initialised: boolean;
+		chatModel: ChatModel;
+		levelState: LevelState[];
+	}
+	interface LevelState {
+		level: LEVEL_NAMES;
+		chatHistory: ChatMessage[];
+		defences: Defence[];
+		sentEmails: EmailInfo[];
+	}
+}
+
+jest.mock('@src/promptTemplates', () => ({
+	systemRoleLevel1: 'systemRoleLevel1',
+	systemRoleLevel2: 'systemRoleLevel2',
+	systemRoleLevel3: 'systemRoleLevel3',
+}));
 
 function responseMock() {
 	return {
@@ -11,10 +37,34 @@ function responseMock() {
 }
 
 test('dummy test', () => {
-	const req;
+	const req = {
+		query: {
+			level: 0,
+		},
+		session: {
+			levelState: [
+				{
+					sentEmails: [],
+					chatHistory: [],
+					defences: [],
+				},
+			],
+			systemRoles: [],
+		},
+	} as unknown as StartGetRequest;
 	const res = responseMock();
 
 	handleStart(req, res);
 
-	expect(true).toBe(true);
+	expect(res.send).toHaveBeenCalledWith({
+		emails: [],
+		chatHistory: [],
+		defences: [],
+		availableModels: [],
+		systemRoles: [
+			{ level: 0, systemRole: 'systemRoleLevel1' },
+			{ level: 1, systemRole: 'systemRoleLevel2' },
+			{ level: 2, systemRole: 'systemRoleLevel3' },
+		],
+	});
 });
