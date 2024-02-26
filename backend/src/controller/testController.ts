62a7a81187130b12f05f018e030cc4ec08d43073
kclark <kclark@scottlogic.com>
2024-02-26 11:01:50 +0000

    linting

--- backend/src/controller/testController.ts
+++ backend/src/controller/testController.ts
@@ -4,8 +4,10 @@ import { handleAddInfoToChatHistory } from '@src/controller/chatController';
 import { OpenAiAddInfoToChatHistoryRequest } from '@src/models/api/OpenAiAddInfoToChatHistoryRequest';
 
 function handleTest(req: OpenAiAddInfoToChatHistoryRequest, res: Response) {
-	const ranNum = Math.round((Math.random()+1)*1000);
-	
-	setTimeout(() => handleAddInfoToChatHistory(req, res), ranNum);
+	const ranNum = Math.round((Math.random() + 1) * 1000);
+
+	setTimeout(() => {
+		handleAddInfoToChatHistory(req, res);
+	}, ranNum);
 }
 export { handleTest };
