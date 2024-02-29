c1fd7efa22544dd44e4ba468c68297e52c9dd079
kclark <kclark@scottlogic.com>
2024-02-29 14:10:39 +0000

    fixing script extra s

--- k6/spike.js
+++ k6/spike.js
@@ -3,7 +3,7 @@ import { postWithSessionUpdate } from './requestFunctions.js';
 export const options = {
 	// Key configurations for spike in this section
 	stages: [
-		{ duration: '2ms', target: 2000 }, // fast ramp-up to a high point
+		{ duration: '2m', target: 2000 }, // fast ramp-up to a high point
 		{ duration: '1m', target: 0 }, // quick ramp-down to 0 users
 	],
 };
