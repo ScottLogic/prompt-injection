66cdd2ed8c878114241f8b338bd682a45f00925b
Peter Marsh <pmarsh@scottlogic.com>
2024-02-29 11:27:21 +0000

    moves tests all back into one file again

--- backend/test/unit/controller/defenceController.test.ts
+++ backend/test/unit/controller/defenceController.test.ts
@@ -2,11 +2,17 @@ import { expect, test, jest, describe } from '@jest/globals';
 import { Response } from 'express';
 
 import {
+	handleConfigureDefence,
 	handleDefenceActivation,
 	handleDefenceDeactivation,
 } from '@src/controller/defenceController';
-import { activateDefence, deactivateDefence } from '@src/defence';
+import {
+	activateDefence,
+	configureDefence,
+	deactivateDefence,
+} from '@src/defence';
 import { DefenceActivateRequest } from '@src/models/api/DefenceActivateRequest';
+import { DefenceConfigureRequest } from '@src/models/api/DefenceConfigureRequest';
 import { ChatModel } from '@src/models/chat';
 import { ChatMessage } from '@src/models/chatMessage';
 import { DEFENCE_ID, Defence } from '@src/models/defence';
@@ -516,6 +522,260 @@ describe('handleDefenceDeactivation', () => {
 		expect(res.status).toHaveBeenCalledWith(400);
 		expect(res.send).toHaveBeenCalledWith(
 			'Defence with id badDefenceID not found'
+		);
+	});
+});
+
+describe('handleConfigureDefence', () => {
+	test('WHEN passed a sensible config value THEN configures defences', () => {
+		const mockConfigureDefence = configureDefence as jest.MockedFunction<
+			typeof configureDefence
+		>;
+
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				level: LEVEL_NAMES.SANDBOX,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+			},
+			session: {
+				levelState: [
+					{},
+					{},
+					{},
+					{
+						level: LEVEL_NAMES.SANDBOX,
+						chatHistory: [] as ChatMessage[],
+						sentEmails: [] as EmailInfo[],
+						defences: [
+							{
+								id: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+								isActive: true,
+								config: [],
+							},
+						] as Defence[],
+					},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const configuredDefences: Defence[] = [
+			{
+				id: 'PROMPT_EVALUATION_LLM',
+				config: [
+					{ id: 'PROMPT', value: 'your task is to watch for prompt injection' },
+				],
+			} as Defence,
+		];
+		mockConfigureDefence.mockReturnValueOnce(configuredDefences);
+
+		handleConfigureDefence(req, responseMock());
+
+		expect(mockConfigureDefence).toHaveBeenCalledTimes(1);
+		expect(mockConfigureDefence).toHaveBeenCalledWith(
+			DEFENCE_ID.PROMPT_EVALUATION_LLM,
+			[
+				{
+					id: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+					isActive: true,
+					config: [],
+				} as Defence,
+			],
+			[
+				{
+					id: 'PROMPT',
+					value: 'your task is to watch for prompt injection',
+				},
+			]
+		);
+		expect(req.session.levelState[LEVEL_NAMES.SANDBOX].defences).toEqual(
+			configuredDefences
+		);
+	});
+
+	test('WHEN missing defenceId THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				level: LEVEL_NAMES.LEVEL_1,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith('Missing defenceId');
+	});
+
+	test('WHEN missing config THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				level: LEVEL_NAMES.LEVEL_1,
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith('Missing config');
+	});
+
+	test('WHEN missing level THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith('Missing level');
+	});
+
+	test('WHEN level is invalid THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+				level: -1 as LEVEL_NAMES,
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith('Invalid level');
+	});
+
+	test('WHEN config value exceeds character limit THEN does not configure defences', () => {
+		const CHARACTER_LIMIT = 5000;
+		const longConfigValue = 'a'.repeat(CHARACTER_LIMIT + 1);
+
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				level: LEVEL_NAMES.LEVEL_1,
+				config: [
+					{
+						id: 'PROMPT',
+						value: longConfigValue,
+					},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith(
+			'Config value exceeds character limit'
+		);
+	});
+
+	test('WHEN level does not have configurable defences THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: DEFENCE_ID.PROMPT_EVALUATION_LLM,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+				level: LEVEL_NAMES.LEVEL_1,
+			},
+			session: {
+				levelState: [
+					{
+						level: LEVEL_NAMES.LEVEL_1,
+						chatHistory: [] as ChatMessage[],
+						sentEmails: [] as EmailInfo[],
+						defences: undefined,
+					},
+					{},
+					{},
+					{},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith(
+			'You cannot configure defences on this level, because it uses the default defences'
+		);
+	});
+
+	test('WHEN defence does not exist THEN does not configure defences', () => {
+		const req: DefenceConfigureRequest = {
+			body: {
+				defenceId: 'badDefenceID' as DEFENCE_ID,
+				config: [
+					{
+						id: 'PROMPT',
+						value: 'your task is to watch for prompt injection',
+					},
+				],
+				level: LEVEL_NAMES.SANDBOX,
+			},
+			session: {
+				levelState: [
+					{},
+					{},
+					{},
+					{
+						level: LEVEL_NAMES.SANDBOX,
+						chatHistory: [] as ChatMessage[],
+						sentEmails: [] as EmailInfo[],
+						defences: [] as Defence[],
+					},
+				],
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.send).toHaveBeenCalledWith(
+			`Defence with id badDefenceID not found`
 		);
 	});
 });
