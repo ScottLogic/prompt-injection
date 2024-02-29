ebba951dc83314113dda3b79a86664f19e128e57
Peter Marsh <pmarsh@scottlogic.com>
2024-02-29 14:32:47 +0000

    adds tests to make sure defences aren't chngeable for level 1

---
+++ backend/test/integration/defenceController.test.ts
@@ -0,0 +1,107 @@
+import { expect, test, jest, describe } from '@jest/globals';
+import { Response } from 'express';
+
+import {
+	handleConfigureDefence,
+	handleDefenceActivation,
+	handleDefenceDeactivation,
+	handleGetDefenceStatus,
+} from '@src/controller/defenceController';
+import { DefenceActivateRequest } from '@src/models/api/DefenceActivateRequest';
+import { DefenceConfigureRequest } from '@src/models/api/DefenceConfigureRequest';
+import { DefenceStatusRequest } from '@src/models/api/DefenceStatusRequest';
+import { DEFENCE_ID } from '@src/models/defence';
+import { LEVEL_NAMES, getInitialLevelStates } from '@src/models/level';
+
+function responseMock() {
+	return {
+		send: jest.fn(),
+		status: jest.fn().mockReturnThis(),
+	} as unknown as Response;
+}
+
+describe('The correct can have their defences changed', () => {
+	describe('Levels 1 and 2', () => {
+		// [LEVEL_NAMES.LEVEL_1, LEVEL_NAMES.LEVEL_2].forEach((level) => {
+
+		// });
+		test('WHEN attempt to activate a defence THEN defence is not activated', () => {
+			const req = {
+				body: {
+					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+					level: LEVEL_NAMES.LEVEL_1,
+				},
+				session: {
+					levelState: getInitialLevelStates(),
+				},
+			} as unknown as DefenceActivateRequest;
+
+			const res = responseMock();
+
+			handleDefenceActivation(req, res);
+
+			expect(res.status).toHaveBeenCalledWith(400);
+			expect(res.send).toHaveBeenCalledWith(
+				'You cannot activate defences on this level, because it uses the default defences'
+			);
+		});
+
+		test('WHEN attempt to deactivate a defence THEN defence is not activated', () => {
+			const req = {
+				body: {
+					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+					level: LEVEL_NAMES.LEVEL_1,
+				},
+				session: {
+					levelState: getInitialLevelStates(),
+				},
+			} as unknown as DefenceActivateRequest;
+
+			const res = responseMock();
+
+			handleDefenceDeactivation(req, res);
+
+			expect(res.status).toHaveBeenCalledWith(400);
+			expect(res.send).toHaveBeenCalledWith(
+				'You cannot deactivate defences on this level, because it uses the default defences'
+			);
+		});
+
+		test('WHEN attempt to configure a defence THEN defence is not configured', () => {
+			const req = {
+				body: {
+					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+					level: LEVEL_NAMES.LEVEL_1,
+					config: [],
+				},
+				session: {
+					levelState: getInitialLevelStates(),
+				},
+			} as unknown as DefenceConfigureRequest;
+
+			const res = responseMock();
+
+			handleConfigureDefence(req, res);
+
+			expect(res.status).toHaveBeenCalledWith(400);
+			expect(res.send).toHaveBeenCalledWith(
+				'You cannot configure defences on this level, because it uses the default defences'
+			);
+		});
+
+		test('WHEN attempt to get defence status THEN shoult return undefined', () => {
+			const req = {
+				query: { level: LEVEL_NAMES.LEVEL_1 },
+				session: {
+					levelState: getInitialLevelStates(),
+				},
+			} as unknown as DefenceStatusRequest;
+
+			const res = responseMock();
+
+			handleGetDefenceStatus(req, res);
+
+			expect(res.send).toHaveBeenCalledWith(undefined);
+		});
+	});
+});
