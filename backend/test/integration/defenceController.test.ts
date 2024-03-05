b45cce96977b041658474a8cedf40bb2604ef08b
Peter Marsh <pmarsh@scottlogic.com>
2024-03-05 09:55:12 +0000

    correct test for configuring a config item

--- backend/test/integration/defenceController.test.ts
+++ backend/test/integration/defenceController.test.ts
@@ -67,54 +67,6 @@ describe('The correct levels can have their defences changed', () => {
 				'You cannot deactivate defences on this level, because it uses the default defences'
 			);
 		});
-
-		test(`GIVEN level ${
-			level + 1
-		} WHEN attempt to configure a defence THEN defence is not configured`, () => {
-			const req = {
-				body: {
-					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
-					level,
-					config: [],
-				},
-				session: {
-					levelState: getInitialLevelStates(),
-				},
-			} as unknown as DefenceConfigureRequest;
-
-			const res = responseMock();
-
-			handleConfigureDefence(req, res);
-
-			expect(res.status).toHaveBeenCalledWith(400);
-			expect(res.send).toHaveBeenCalledWith(
-				'You cannot configure defences on this level, because it uses the default defences'
-			);
-		});
-
-		test(`GIVEN level ${
-			level + 1
-		} WHEN attempt to reset a defence config item THEN defence config item is not reset`, () => {
-			const req = {
-				body: {
-					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
-					level,
-					configItemId: 'MAX_MESSAGE_LENGTH',
-				},
-				session: {
-					levelState: getInitialLevelStates(),
-				},
-			} as DefenceConfigItemResetRequest;
-
-			const res = responseMock();
-
-			handleResetDefenceConfigItem(req, res);
-
-			expect(res.status).toHaveBeenCalledWith(400);
-			expect(res.send).toHaveBeenCalledWith(
-				'You cannot reset defence config items on this level, because it uses the default defences'
-			);
-		});
 	});
 
 	[LEVEL_NAMES.LEVEL_3, LEVEL_NAMES.SANDBOX].forEach((level) => {
@@ -184,75 +136,125 @@ describe('The correct levels can have their defences changed', () => {
 			expect(res.send).toHaveBeenCalled();
 			expect(req.session.levelState).toEqual(getInitialLevelStates());
 		});
+	});
 
-		test(`GIVEN level ${level} WHEN attempt to configure a defence THEN defence is configured`, () => {
-			const req = {
-				body: {
-					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
-					level,
-					config: [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }],
-				},
-				session: {
-					levelState: getInitialLevelStates(),
-				},
-			} as DefenceConfigureRequest;
+	[LEVEL_NAMES.LEVEL_1, LEVEL_NAMES.LEVEL_2, LEVEL_NAMES.LEVEL_3].forEach(
+		(level) => {
+			test(`GIVEN level ${
+				level + 1
+			} WHEN attempt to configure a defence THEN defence is not configured`, () => {
+				const req = {
+					body: {
+						defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+						level,
+						config: [],
+					},
+					session: {
+						levelState: getInitialLevelStates(),
+					},
+				} as unknown as DefenceConfigureRequest;
 
-			const res = responseMock();
+				const res = responseMock();
 
-			handleConfigureDefence(req, res);
+				handleConfigureDefence(req, res);
 
-			const updatedDefenceConfig = req.session.levelState
-				.find((levelState) => levelState.level === level)
-				?.defences?.find(
-					(defence) => defence.id === DEFENCE_ID.CHARACTER_LIMIT
-				)?.config;
-
-			const expectedDefenceConfig = [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }];
-
-			expect(res.send).toHaveBeenCalled();
-			expect(updatedDefenceConfig).toEqual(expectedDefenceConfig);
-		});
-
-		test(`GIVEN level ${
-			level + 1
-		} WHEN attempt to reset a defence config item THEN defence config item is reset`, () => {
-			const initialLevelStatesButWithCharacterLimitConfigured =
-				getInitialLevelStates().map((levelState) =>
-					levelState.level === level
-						? {
-								...levelState,
-								defences: levelState.defences?.map((defence) =>
-									defence.id === DEFENCE_ID.CHARACTER_LIMIT
-										? {
-												...defence,
-												config: [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }],
-										  }
-										: defence
-								),
-						  }
-						: levelState
+				expect(res.status).toHaveBeenCalledWith(400);
+				expect(res.send).toHaveBeenCalledWith(
+					'You cannot configure defences on this level, because it uses the default defences'
 				);
-
-			const req = {
-				body: {
-					defenceId: DEFENCE_ID.CHARACTER_LIMIT,
-					level,
-					configItemId: 'MAX_MESSAGE_LENGTH',
-				},
-				session: {
-					levelState: initialLevelStatesButWithCharacterLimitConfigured,
-				},
-			} as DefenceConfigItemResetRequest;
-
-			const res = responseMock();
-
-			handleResetDefenceConfigItem(req, res);
-
-			expect(req.session.levelState).toEqual(getInitialLevelStates());
-			expect(res.send).toHaveBeenCalledWith({
-				id: 'MAX_MESSAGE_LENGTH',
-				value: '280',
 			});
+
+			test(`GIVEN level ${
+				level + 1
+			} WHEN attempt to reset a defence config item THEN defence config item is not reset`, () => {
+				const req = {
+					body: {
+						defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+						level,
+						configItemId: 'MAX_MESSAGE_LENGTH',
+					},
+					session: {
+						levelState: getInitialLevelStates(),
+					},
+				} as DefenceConfigItemResetRequest;
+
+				const res = responseMock();
+
+				handleResetDefenceConfigItem(req, res);
+
+				expect(res.status).toHaveBeenCalledWith(400);
+				expect(res.send).toHaveBeenCalledWith(
+					'You cannot reset defence config items on this level, because it uses the default defences'
+				);
+			});
+		}
+	);
+
+	test(`GIVEN level sandbox WHEN attempt to configure a defence THEN defence is configured`, () => {
+		const req = {
+			body: {
+				defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+				level: LEVEL_NAMES.SANDBOX,
+				config: [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }],
+			},
+			session: {
+				levelState: getInitialLevelStates(),
+			},
+		} as DefenceConfigureRequest;
+
+		const res = responseMock();
+
+		handleConfigureDefence(req, res);
+
+		const updatedDefenceConfig = req.session.levelState
+			.find((levelState) => levelState.level === LEVEL_NAMES.SANDBOX)
+			?.defences?.find(
+				(defence) => defence.id === DEFENCE_ID.CHARACTER_LIMIT
+			)?.config;
+
+		const expectedDefenceConfig = [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }];
+
+		expect(res.send).toHaveBeenCalled();
+		expect(updatedDefenceConfig).toEqual(expectedDefenceConfig);
+	});
+
+	test(`GIVEN level Sandbox WHEN attempt to reset a defence config item THEN defence config item is reset`, () => {
+		const initialLevelStatesButWithCharacterLimitConfigured =
+			getInitialLevelStates().map((levelState) =>
+				levelState.level === LEVEL_NAMES.SANDBOX
+					? {
+							...levelState,
+							defences: levelState.defences?.map((defence) =>
+								defence.id === DEFENCE_ID.CHARACTER_LIMIT
+									? {
+											...defence,
+											config: [{ id: 'MAX_MESSAGE_LENGTH', value: '1' }],
+									  }
+									: defence
+							),
+					  }
+					: levelState
+			);
+
+		const req = {
+			body: {
+				defenceId: DEFENCE_ID.CHARACTER_LIMIT,
+				level: LEVEL_NAMES.SANDBOX,
+				configItemId: 'MAX_MESSAGE_LENGTH',
+			},
+			session: {
+				levelState: initialLevelStatesButWithCharacterLimitConfigured,
+			},
+		} as DefenceConfigItemResetRequest;
+
+		const res = responseMock();
+
+		handleResetDefenceConfigItem(req, res);
+
+		expect(req.session.levelState).toEqual(getInitialLevelStates());
+		expect(res.send).toHaveBeenCalledWith({
+			id: 'MAX_MESSAGE_LENGTH',
+			value: '280',
 		});
 	});
 });
