bb850e539d08e905fab28d7e164342cdd88d49cc
Chris Wilton-Magras <chriswilty@zoho.com>
2024-03-15 14:08:15 +0000

    806, 809: Route 53 and Certificates for spylogic.ai (#860)
    * Add DNS and certificate to cloudfront and fargate

--- cloud/bin/cloud.ts
+++ cloud/bin/cloud.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { App, Environment } from 'aws-cdk-lib';
+import { App, Environment } from 'aws-cdk-lib/core';
 import 'source-map-support/register';
 
 import {
@@ -9,10 +9,16 @@ import {
 	stackName,
 	ApiStack,
 	AuthStack,
+	CertificateStack,
+	HostedZoneStack,
 	UiStack,
 } from '../lib';
 
 const app = new App();
+const generateStackName = stackName(app);
+const generateDescription = resourceDescription(app);
+
+/* Common stack resources */
 
 const env: Environment = {
 	account: process.env.CDK_DEFAULT_ACCOUNT,
@@ -23,31 +29,55 @@ const tags = {
 	owner: appName,
 	classification: 'unrestricted',
 	'environment-type': environmentName(app),
-	'keep-alive': '8-6-without-weekends',
+	'keep-alive': '9-5-without-weekends',
 };
 
-const generateStackName = stackName(app);
-const generateDescription = resourceDescription(app);
+/* Stack constructs */
+
+const hostedZoneStack = new HostedZoneStack(
+	app,
+	generateStackName('hosted-zone'),
+	{
+		description: generateDescription('Hosted Zone stack'),
+		env,
+		tags,
+	}
+);
+
+const certificateStack = new CertificateStack(
+	app,
+	generateStackName('certificate'),
+	{
+		description: generateDescription('Certificate stack'),
+		env,
+		tags,
+		hostedZone: hostedZoneStack.hostedZone,
+	}
+);
 
 const uiStack = new UiStack(app, generateStackName('ui'), {
 	description: generateDescription('UI stack'),
 	env,
 	tags,
+	certificate: certificateStack.cloudFrontCert,
+	hostedZone: hostedZoneStack.hostedZone,
 });
 
 /*const authStack = */ new AuthStack(app, generateStackName('auth'), {
 	description: generateDescription('Auth stack'),
 	env,
 	tags,
-	webappUrl: uiStack.cloudfrontUrl,
+	webappUrl: uiStack.cloudFrontUrl,
 });
 
 new ApiStack(app, generateStackName('api'), {
 	description: generateDescription('API stack'),
 	env,
 	tags,
+	certificate: certificateStack.loadBalancerCert,
+	hostedZone: hostedZoneStack.hostedZone,
 	// userPool: authStack.userPool,
 	// userPoolClient: authStack.userPoolClient,
 	// userPoolDomain: authStack.userPoolDomain,
-	webappUrl: uiStack.cloudfrontUrl,
+	webappUrl: uiStack.cloudFrontUrl,
 });
