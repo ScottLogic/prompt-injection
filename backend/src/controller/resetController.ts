bb94bd742df41a31b2dbb548d55a4b97eed91b58
Peter Marsh <pmarsh@scottlogic.com>
2024-03-19 09:51:37 +0000

    resetController now returns the relevant stuff for the reset level

--- backend/src/controller/resetController.ts
+++ backend/src/controller/resetController.ts
@@ -1,11 +1,34 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
-import { getInitialLevelStates } from '@src/models/level';
+import { LevelGetRequest } from '@src/models/api/LevelGetRequest';
+import {
+	LEVEL_NAMES,
+	getInitialLevelStates,
+	isValidLevel,
+} from '@src/models/level';
 
-function handleResetProgress(req: Request, res: Response) {
+function handleResetProgress(req: LevelGetRequest, res: Response) {
+	const { level } = req.query;
+
+	if (level === undefined) {
+		res.status(400).send('Level not provided');
+		return;
+	}
+
+	if (!isValidLevel(level)) {
+		res.status(400).send('Invalid level');
+		return;
+	}
+
 	console.debug('Resetting progress for all levels', req.session.levelState);
 	req.session.levelState = getInitialLevelStates();
-	res.send(req.session.levelState);
+	res.send({
+		emails: req.session.levelState[level].sentEmails,
+		chatHistory: req.session.levelState[level].chatHistory,
+		defences: req.session.levelState[level].defences,
+		chatModel:
+			level === LEVEL_NAMES.SANDBOX ? req.session.chatModel : undefined,
+	});
 }
 
 export { handleResetProgress };
