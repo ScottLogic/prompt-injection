6e8cb662af581ea227716b4231717ad0f079b508
Peter Marsh <pmarsh@scottlogic.com>
2024-03-28 10:33:41 +0000

    chatMessage is not generated in backend

--- backend/src/models/api/OpenAiSetModelRequest.ts
+++ backend/src/models/api/OpenAiSetModelRequest.ts
@@ -1,10 +1,13 @@
 import { Request } from 'express';
 
 import { CHAT_MODEL_ID, ChatModelConfigurations } from '@src/models/chat';
+import { ChatMessage } from '@src/models/chatMessage';
 
 export type OpenAiSetModelRequest = Request<
 	never,
-	never,
+	{
+		chatInfoMessage: ChatMessage;
+	},
 	{
 		model?: CHAT_MODEL_ID;
 		configuration?: ChatModelConfigurations;
