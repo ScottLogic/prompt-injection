303fd5ffd2999344b796a0db0623b9d58fd50630
Peter Marsh <pmarsh@scottlogic.com>
2024-03-28 15:23:19 +0000

    removes legacy models

--- backend/src/models/chat.ts
+++ backend/src/models/chat.ts
@@ -12,9 +12,6 @@ const chatModelIds = [
 	'gpt-4',
 	'gpt-4-0613',
 	'gpt-3.5-turbo',
-	'gpt-3.5-turbo-0613',
-	'gpt-3.5-turbo-16k',
-	'gpt-3.5-turbo-16k-0613',
 ] as const;
 
 type CHAT_MODEL_ID = (typeof chatModelIds)[number];
