eafe911c159a1f17777b141826365e7b121c074e
Chris Wilton-Magras <chriswilty@zoho.com>
2024-04-24 09:29:23 +0100

    902: Use ALL vectorstore documents in QA LLM context (#903)

--- backend/src/document.ts
+++ backend/src/document.ts
@@ -1,7 +1,9 @@
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { CSVLoader } from 'langchain/document_loaders/fs/csv';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import {
+	DirectoryLoader,
+	UnknownHandling,
+} from 'langchain/document_loaders/fs/directory';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
@@ -14,25 +16,20 @@ import { LEVEL_NAMES } from './models/level';
 async function getDocuments(filePath: string) {
 	console.debug(`Loading documents from: ${filePath}`);
 
-	const loader: DirectoryLoader = new DirectoryLoader(filePath, {
-		'.pdf': (path: string) => new PDFLoader(path),
-		'.txt': (path: string) => new TextLoader(path),
-		'.csv': (path: string) => new CSVLoader(path),
-	});
+	const loader: DirectoryLoader = new DirectoryLoader(
+		filePath,
+		{
+			'.txt': (path: string) => new TextLoader(path),
+			// CSV loader returns one document per row
+			'.csv': (path: string) => new CSVLoader(path),
+		},
+		true,
+		UnknownHandling.Warn
+	);
 	const docs = await loader.load();
 	console.debug(`${docs.length} documents found`);
 
-	// split the documents into chunks
-	const textSplitter = new RecursiveCharacterTextSplitter({
-		chunkSize: 1000,
-		chunkOverlap: 0,
-	});
-
-	// need to disable the linting rule here
-	// as it's possible for the splitDocuments method to return undefined
-	// despite what the return type says
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	return (await textSplitter.splitDocuments(docs)) ?? [];
+	return new RecursiveCharacterTextSplitter().splitDocuments(docs);
 }
 
 function getFilepath(target: LEVEL_NAMES | 'common') {
