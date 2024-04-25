2f5a9dc1b837714f819d2f06f29a194302c9341b
Chris Wilton-Magras <chriswilty@zoho.com>
2024-04-25 14:32:52 +0100

    808: Authenticate backend via cloudfront function (#904)
    * Prevent unauthorized API access using a CloudFront proxy
    * Make UI auth opt-in
    * Correct document loading and cache control

--- cloud/lib/ui-stack.ts
+++ cloud/lib/ui-stack.ts
@@ -1,59 +1,66 @@
+import { TypeScriptCode } from '@mrgrain/cdk-esbuild';
 import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import {
 	AllowedMethods,
 	CacheCookieBehavior,
 	CachePolicy,
 	Distribution,
+	experimental,
+	LambdaEdgeEventType,
 	OriginAccessIdentity,
-	SecurityPolicyProtocol,
+	OriginRequestPolicy,
+	PriceClass,
+	ResponseHeadersPolicy,
 	ViewerProtocolPolicy,
 } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import {
-	CfnOutput,
-	Duration,
-	RemovalPolicy,
-	Stack,
-	StackProps,
-} from 'aws-cdk-lib/core';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import {
-	AaaaRecord,
-	ARecord,
-	IHostedZone,
-	RecordTarget,
-} from 'aws-cdk-lib/aws-route53';
+import { HttpOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CanonicalUserPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { AaaaRecord, ARecord, IHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
-import {
-	BlockPublicAccess,
-	Bucket,
-	BucketEncryption,
-} from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import { join } from 'node:path';
 
-import { appName, resourceName } from './resourceNamingUtils';
+import { appName, resourceId } from './resourceNamingUtils';
 
 type UiStackProps = StackProps & {
+	apiDomainName: string;
 	certificate: ICertificate;
+	customAuthHeaderName: string;
+	customAuthHeaderValue: string;
 	hostedZone: IHostedZone;
+	parameterNameUserPoolClient: string;
+	parameterNameUserPoolId: string;
 };
 
 export class UiStack extends Stack {
-	public readonly cloudFrontUrl: string;
-
 	constructor(scope: Construct, id: string, props: UiStackProps) {
 		super(scope, id, props);
 
-		const generateResourceName = resourceName(scope);
-		const { certificate, hostedZone } = props;
+		const generateResourceId = resourceId(scope);
+		const {
+			apiDomainName,
+			certificate,
+			customAuthHeaderName,
+			customAuthHeaderValue,
+			env,
+			hostedZone,
+			parameterNameUserPoolClient,
+			parameterNameUserPoolId,
+		} = props;
 
-		const cloudfrontOAI = new OriginAccessIdentity(
-			this,
-			generateResourceName('cloudfront-OAI')
-		);
+		if (!env?.region) {
+			throw new Error('Region not defined in stack env, cannot continue!');
+		}
 
-		// Host Bucket
-		const bucketName = generateResourceName('host-bucket');
+		const cloudfrontOAI = new OriginAccessIdentity(this, generateResourceId('cloudfront-OAI'));
+
+		/*
+		UI Host Bucket
+		*/
+		const bucketName = generateResourceId('host-bucket');
 		const hostBucket = new Bucket(this, bucketName, {
 			bucketName,
 			blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
@@ -63,71 +70,143 @@ export class UiStack extends Stack {
 			autoDeleteObjects: true,
 		});
 		hostBucket.addToResourcePolicy(
-			new iam.PolicyStatement({
+			new PolicyStatement({
 				actions: ['s3:GetObject'],
 				resources: [hostBucket.arnForObjects('*')],
 				principals: [
-					new iam.CanonicalUserPrincipal(
-						cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
-					),
+					new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId),
 				],
 			})
 		);
 
-		// CloudFront Distribution
-		const cachePolicyName = generateResourceName('site-cache-policy');
-		const cloudFrontDistribution = new Distribution(
+		/*
+		Edge lambda as JWT token verifier, to check request has access token
+			- YES and valid: add custom header with UUID that ALB will filter on
+			- NO or invalid: return 401 Unauthorized
+
+		TODO
+		Once we have a pipeline, could we inject the jwks.json during a build step?
+		If so, we might be able to switch to a CloudFront Function instead of Edge,
+		and use CloudFront KeyValueStore to hold our jwks value as JSON.
+		*/
+		const verifierEdgeFunction = new experimental.EdgeFunction(
 			this,
-			generateResourceName('site-distribution'),
+			generateResourceId('api-gatekeeper'),
 			{
-				defaultRootObject: 'index.html',
-				minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
-				certificate,
-				domainNames: [hostedZone.zoneName],
-				errorResponses: [
-					{
-						httpStatus: 404,
-						responseHttpStatus: 200,
-						responsePagePath: '/index.html',
-						ttl: Duration.seconds(30),
+				functionName: 'edge-api-gatekeeper',
+				handler: 'index.handler',
+				runtime: Runtime.NODEJS_18_X,
+				code: new TypeScriptCode(join(__dirname, 'lambdas/verifyAuth/index.ts'), {
+					buildOptions: {
+						bundle: true,
+						external: ['@aws-sdk/client-ssm'],
+						minify: false,
+						platform: 'node',
+						target: 'node18',
+						define: {
+							'process.env.DOMAIN_NAME': `"${hostedZone.zoneName}"`,
+							'process.env.PARAM_USERPOOL_ID': `"${parameterNameUserPoolId}"`,
+							'process.env.PARAM_USERPOOL_CLIENT': `"${parameterNameUserPoolClient}"`,
+						},
 					},
-					// TODO Do we want a custom page for 503, for when server is down?
-				],
-				defaultBehavior: {
-					origin: new S3Origin(hostBucket, {
-						originAccessIdentity: cloudfrontOAI,
-					}),
-					cachePolicy: new CachePolicy(this, cachePolicyName, {
-						cachePolicyName,
-						cookieBehavior: CacheCookieBehavior.allowList(`${appName}.sid`),
-					}),
-					compress: true,
-					allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-					viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-				},
+				}),
 			}
 		);
-
-		// DNS Records for Route53
-		const target = RecordTarget.fromAlias(
-			new CloudFrontTarget(cloudFrontDistribution)
+		verifierEdgeFunction.addToRolePolicy(
+			new PolicyStatement({
+				effect: Effect.ALLOW,
+				actions: ['ssm:GetParameters'],
+				resources: ['*'],
+			})
 		);
-		new ARecord(this, generateResourceName('a-record-cfront'), {
-			zone: hostedZone,
-			target,
-			deleteExisting: true,
-			comment: 'DNS A Record for the UI host',
+
+		/*
+		Main CloudFront distribution
+
+		Includes API proxy to verify user is authorized and add a custom header; load balancer
+		can then use this as a filter, to only accept authorized requests from CloudFront.
+		*/
+		const albOrigin = new HttpOrigin(apiDomainName, {
+			customHeaders: {
+				[customAuthHeaderName]: customAuthHeaderValue,
+			},
 		});
-		new AaaaRecord(this, generateResourceName('aaaa-record-cfront'), {
-			zone: hostedZone,
-			target,
-			deleteExisting: true,
-			comment: 'DNS AAAA Record for the UI host',
+		const siteDistribution = new Distribution(this, generateResourceId('site-distribution'), {
+			defaultRootObject: 'index.html',
+			certificate,
+			domainNames: [hostedZone.zoneName],
+			enableLogging: true,
+			errorResponses: [
+				{
+					httpStatus: 404,
+					responseHttpStatus: 200,
+					responsePagePath: '/index.html',
+					ttl: Duration.seconds(30),
+				},
+			],
+			defaultBehavior: {
+				origin: new S3Origin(hostBucket, {
+					originAccessIdentity: cloudfrontOAI,
+				}),
+				cachePolicy: new CachePolicy(this, generateResourceId('site-cache-policy'), {
+					cookieBehavior: CacheCookieBehavior.allowList(`${appName}.sid`),
+				}),
+				compress: true,
+				allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+				viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+			},
+			additionalBehaviors: {
+				'/api/documents/*': {
+					// Cache static content, currently just docs
+					origin: albOrigin,
+					cachePolicy: new CachePolicy(this, generateResourceId('static-resources'), {
+						comment: 'Long-lived cache for static files, invalidation forced after redeploy',
+						minTtl: Duration.days(14),
+					}),
+					viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+					originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+					responseHeadersPolicy: ResponseHeadersPolicy.SECURITY_HEADERS,
+					edgeLambdas: [
+						{
+							functionVersion: verifierEdgeFunction.currentVersion,
+							eventType: LambdaEdgeEventType.VIEWER_REQUEST,
+						},
+					],
+				},
+				'/api/*': {
+					// Treat all other paths as dynamic, do not cache
+					origin: albOrigin,
+					allowedMethods: AllowedMethods.ALLOW_ALL,
+					cachePolicy: CachePolicy.CACHING_DISABLED,
+					viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+					originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+					responseHeadersPolicy: ResponseHeadersPolicy.SECURITY_HEADERS,
+					edgeLambdas: [
+						{
+							functionVersion: verifierEdgeFunction.currentVersion,
+							eventType: LambdaEdgeEventType.VIEWER_REQUEST,
+						},
+					],
+				},
+			},
+			priceClass: PriceClass.PRICE_CLASS_100,
 		});
 
-		this.cloudFrontUrl = `https://${hostedZone.zoneName}`;
-		new CfnOutput(this, 'WebURL', {
-			value: this.cloudFrontUrl,
+		/*
+		DNS Records for Route53
+		*/
+		const websiteTarget = RecordTarget.fromAlias(new CloudFrontTarget(siteDistribution));
+		new ARecord(this, generateResourceId('arecord-cfront'), {
+			zone: hostedZone,
+			target: websiteTarget,
+			deleteExisting: true,
+			comment: 'DNS A Record for UI host',
+		});
+		new AaaaRecord(this, generateResourceId('aaaarecord-cfront'), {
+			zone: hostedZone,
+			target: websiteTarget,
+			deleteExisting: true,
+			comment: 'DNS AAAA Record for UI host',
 		});
 	}
 }
