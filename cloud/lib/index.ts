c3038521aabf25efa7ed088bb416b3776863e703
Chris Wilton-Magras <chriswilty@zoho.com>
2024-07-26 14:36:48 +0100

    Add another CDK app to test synth and deploy

--- cloud/lib/index.ts
+++ cloud/lib/index.ts
@@ -1,3 +1,10 @@
 export * from './resourceNamingUtils';
+
+export { ApiStack } from './api-stack';
+export { AuthStack } from './auth-stack';
+export { CertificateStack } from './certificate-stack';
+export { HostedZoneStack } from './hostedzone-stack';
+export { UiStack } from './ui-stack';
+
 export { PipelineAssistUsEast1Stack } from './pipeline-assist-useast1-stack';
 export { PipelineStack } from './pipeline-stack';
